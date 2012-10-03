4715ba1e1744880dd9c3ac5db1565d66e409c641
Andre Rodrigues <andrerod@microsoft.com>
2012-10-03 12:56:40 +0100

    Updating setAzureWebsite

--- WindowsAzurePowershell/src/Management.WebSites/Cmdlets/SetAzureWebsite.cs
+++ WindowsAzurePowershell/src/Management.WebSites/Cmdlets/SetAzureWebsite.cs
@@ -12,6 +12,12 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.ServiceModel;
+using Microsoft.WindowsAzure.Management.Websites.Properties;
+using Microsoft.WindowsAzure.Management.Websites.Services;
+using Microsoft.WindowsAzure.Management.Websites.Services.WebEntities;
+
 namespace Microsoft.WindowsAzure.Management.Websites.Cmdlets
 {
     using WebSites.Cmdlets.Common;
@@ -23,17 +29,115 @@ namespace Microsoft.WindowsAzure.Management.Websites.Cmdlets
     [Cmdlet(VerbsCommon.Set, "AzureWebsite")]
     public class SetAzureWebsite : WebsiteContextBaseCmdlet
     {
-        [Parameter(Position = 2, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Number of instances.")]
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Number of workers.")]
         [ValidateNotNullOrEmpty]
-        public int? Instances
-        {
-            get;
-            set;
-        }
+        public int? NumberOfWorkers { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Default Documents.")]
+        [ValidateNotNullOrEmpty]
+        public string[] DefaultDocuments { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = ".NET framework version.")]
+        [ValidateNotNullOrEmpty]
+        public string NetFrameworkVersion { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "PHP Version.")]
+        [ValidateNotNullOrEmpty]
+        public string PhpVersion { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Request tracing enabled.")]
+        [ValidateNotNullOrEmpty]
+        public bool? RequestTracingEnabled { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "HTTP Logging enabled.")]
+        [ValidateNotNullOrEmpty]
+        public bool? HttpLoggingEnabled { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Detailed Error Logging enabled.")]
+        [ValidateNotNullOrEmpty]
+        public bool? DetailedErrorLoggingEnabled { get; set; }
 
         internal override void ExecuteCommand()
         {
+            Site website = null;
+            SiteConfig websiteConfig = null;
+            InvokeInOperationContext(() =>
+            {
+                try
+                {
+                    website = RetryCall(s => Channel.GetSite(s, Name, null));
+                    websiteConfig = RetryCall(s => Channel.GetSiteConfig(s, Name, null));
+                }
+                catch (CommunicationException ex)
+                {
+                    WriteErrorDetails(ex);
+                }
+            });
 
+            if (website == null)
+            {
+                throw new Exception(string.Format(Resources.InvalidWebsite, Name));
+            }
+
+            SiteConfig websiteUpdate = new SiteConfig();
+            
+            bool changes = false;
+            if (NumberOfWorkers != null && !NumberOfWorkers.Equals(websiteConfig.NumberOfWorkers))
+            {
+                changes = true;
+                websiteUpdate.NumberOfWorkers = NumberOfWorkers;
+            }
+
+            if (DefaultDocuments != null && !DefaultDocuments.Equals(websiteConfig.DefaultDocuments))
+            {
+                changes = true;
+                websiteUpdate.DefaultDocuments = DefaultDocuments;
+            }
+       
+            if (NetFrameworkVersion != null && !NetFrameworkVersion.Equals(websiteConfig.NetFrameworkVersion))
+            {
+                changes = true;
+                websiteUpdate.NetFrameworkVersion = NetFrameworkVersion;
+            }
+
+            if (PhpVersion != null && !PhpVersion.Equals(websiteConfig.PhpVersion))
+            {
+                changes = true;
+                websiteUpdate.PhpVersion = PhpVersion;
+            }
+
+            if (RequestTracingEnabled != null && !RequestTracingEnabled.Equals(websiteConfig.RequestTracingEnabled))
+            {
+                changes = true;
+                websiteUpdate.RequestTracingEnabled = RequestTracingEnabled;
+            }
+
+            if (HttpLoggingEnabled != null && !HttpLoggingEnabled.Equals(websiteConfig.HttpLoggingEnabled))
+            {
+                changes = true;
+                websiteUpdate.HttpLoggingEnabled = HttpLoggingEnabled;
+            }
+
+            if (DetailedErrorLoggingEnabled != null && !DetailedErrorLoggingEnabled.Equals(websiteConfig.DetailedErrorLoggingEnabled))
+            {
+                changes = true;
+                websiteUpdate.DetailedErrorLoggingEnabled = DetailedErrorLoggingEnabled;
+            }
+
+            if (changes)
+            {
+                InvokeInOperationContext(() =>
+                {
+                    try
+                    {
+                        RetryCall(s => Channel.UpdateSiteConfig(s, website.WebSpace, Name, websiteUpdate));
+                    }
+                    catch (CommunicationException ex)
+                    {
+                        WriteErrorDetails(ex);
+                    }
+                });
+            }
         }
     }
 }
