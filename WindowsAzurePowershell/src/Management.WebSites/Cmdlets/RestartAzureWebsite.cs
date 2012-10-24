d8f996f5c85334179ff916210c7ef624f12538c1
Chris Tavares <ctavares@microsoft.com>
2012-10-23 17:16:06 -0700

    Responding to code review feedback.

--- WindowsAzurePowershell/src/Management.WebSites/Cmdlets/RestartAzureWebsite.cs
+++ WindowsAzurePowershell/src/Management.WebSites/Cmdlets/RestartAzureWebsite.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.WindowsAzure.Management.WebSites.Cmdlets.Common;
-using System.Management.Automation;
-using Microsoft.WindowsAzure.Management.Websites.Services;
-using Microsoft.WindowsAzure.Management.Websites.Properties;
-using Microsoft.WindowsAzure.Management.Websites.Services.WebEntities;
-using Microsoft.WindowsAzure.Management.Utilities;
-using System.ServiceModel;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright 2011 Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
 
 namespace Microsoft.WindowsAzure.Management.Websites.Cmdlets
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.WindowsAzure.Management.WebSites.Cmdlets.Common;
+    using System.Management.Automation;
+    using Microsoft.WindowsAzure.Management.Websites.Services;
+    using Microsoft.WindowsAzure.Management.Websites.Properties;
+    using Microsoft.WindowsAzure.Management.Websites.Services.WebEntities;
+    using Microsoft.WindowsAzure.Management.Utilities;
+    using System.ServiceModel;
+
+    
     [Cmdlet(VerbsLifecycle.Restart, "AzureWebsite")]
     public class RestartAzureWebsiteCommand : WebsiteContextBaseCmdlet
     {
