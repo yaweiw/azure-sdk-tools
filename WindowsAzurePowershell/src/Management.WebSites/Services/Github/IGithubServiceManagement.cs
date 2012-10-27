de89c199ef8eff1d545b3bb8eae34245ce1d338e
Andre Rodrigues <andrerod@microsoft.com>
2012-10-27 13:50:25 +0100

    Scaffolding github linking.

---
+++ WindowsAzurePowershell/src/Management.WebSites/Services/Github/IGithubServiceManagement.cs
@@ -0,0 +1,37 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright 2011 Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Management.Websites.Services.Github
+{
+    using System;
+    using System.ComponentModel;
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+    using WebEntities;
+
+    /// <summary>
+    /// Provides the Github Api. 
+    /// </summary>
+    [ServiceContract]
+    public interface IGithubServiceManagement
+    {
+        [Description("Gets the members for an organization")]
+        [OperationContract(AsyncPattern = true)]
+        [WebInvoke(Method = "GET", UriTemplate = "/orgs/{organization}/members/")]
+        IAsyncResult BeginGetOrganizationMembers(string organization, AsyncCallback callback, object state);
+        WebSpaces EndGetOrganizationMembers(IAsyncResult asyncResult);
+
+
+    }
+}
---
+++ WindowsAzurePowershell/src/Management.WebSites/Services/LinkedRevisionControl.cs
@@ -0,0 +1,58 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright 2011 Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Management.Websites.Services
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public abstract class LinkedRevisionControl
+    {
+        public abstract void Init();
+        public abstract void Deploy();
+
+        public static LinkedRevisionControl CreateClient(string client)
+        {
+            switch(client)
+            {
+                case "github":
+                    return null;
+                    break;
+                case "git":
+                    return null;
+                    break;  
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        internal bool IsGitWorkingTree()
+        {
+            return Git.GetWorkingTree().Any(line => line.Equals(".git"));
+        }
+
+        internal void InitGitOnCurrentDirectory(string invocationPath)
+        {
+            Git.InitRepository();
+
+            if (!File.Exists(".gitignore"))
+            {
+                // Scaffold gitignore
+                string cmdletPath = Directory.GetParent(invocationPath).FullName;
+                File.Copy(Path.Combine(cmdletPath, "Resources/Scaffolding/Node/.gitignore"), ".gitignore");
+            }
+        }
+    }
+}
