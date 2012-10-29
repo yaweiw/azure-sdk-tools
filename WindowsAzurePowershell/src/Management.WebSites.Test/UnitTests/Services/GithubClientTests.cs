6f81e0a45c36890c021da75cdce90047670c8fe8
Andre Rodrigues <andrerod@microsoft.com>
2012-10-29 11:35:18 +0000

    Adding UT.

--- WindowsAzurePowershell/src/Management.WebSites.Test/UnitTests/Services/GithubClientTests.cs
+++ WindowsAzurePowershell/src/Management.WebSites.Test/UnitTests/Services/GithubClientTests.cs
@@ -14,6 +14,15 @@
 
 namespace Microsoft.WindowsAzure.Management.Websites.Test.UnitTests.Services
 {
+    using Microsoft.WindowsAzure.Management.Websites.Cmdlets.Common;
+    using Microsoft.WindowsAzure.Management.Websites.Services.Github;
+    using Microsoft.WindowsAzure.Management.Websites.Services.Github.Entities;
+    using Microsoft.WindowsAzure.Management.Websites.Test.UnitTests.Utilities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Management.Automation.Host;
     using VisualStudio.TestTools.UnitTesting;
     using Websites.Services;
 
@@ -23,7 +32,71 @@ namespace Microsoft.WindowsAzure.Management.Websites.Test.UnitTests.Services
         [TestMethod]
         public void TestGetRepositories()
         {
+            // Setup
+            SimpleGithubManagement channel = new SimpleGithubManagement();
+
+            channel.GetRepositoriesThunk = ar => new List<GithubRepository> { new GithubRepository { Name = "userrepo1" } };
+            channel.GetOrganizationsThunk = ar => new List<GithubOrganization> { new GithubOrganization { Login = "org1" }, new GithubOrganization { Login = "org2" } };
+            channel.GetRepositoriesFromOrgThunk = ar => 
+            {
+                if (ar.Values["organization"] == "org1")
+                {
+                    return new List<GithubRepository> { new GithubRepository { Name = "org1repo1" } };
+                }
+                else if (ar.Values["organization"] == "org2")
+                {
+                    return new List<GithubRepository> { new GithubRepository { Name = "org2repo1" } };
+                }
+
+                return new List<GithubRepository> { new GithubRepository { Name = "other" } };
+            };
+
+
+            // Test
+            CmdletAccessor cmdletAccessor = new CmdletAccessor();
+            cmdletAccessor.GithubChannel = channel;
             
+            GithubClientAccessor githubClientAccessor = new GithubClientAccessor(cmdletAccessor, null, null, null);
+            var repositories = githubClientAccessor.GetRepositoriesAccessor();
+
+            Assert.AreEqual(3, repositories.Count);
+            Assert.IsNotNull(repositories.FirstOrDefault(r => r.Name.Equals("userrepo1")));
+            Assert.IsNotNull(repositories.FirstOrDefault(r => r.Name.Equals("org1repo1")));
+            Assert.IsNotNull(repositories.FirstOrDefault(r => r.Name.Equals("org2repo1")));
+        }
+    }
+
+    internal class GithubClientAccessor : GithubClient
+    {
+        public GithubClientAccessor(IGithubCmdlet pscmdlet, string githubUsername, string githubPassword, string githubRepository)
+            : base (pscmdlet, githubUsername, githubPassword, githubRepository)
+        {
+        }
+
+        public IList<GithubRepository> GetRepositoriesAccessor()
+        {
+            return GetRepositories();
+        }
+    }
+
+    internal class CmdletAccessor : IGithubCmdlet
+    {
+        public IGithubServiceManagement GithubChannel { get; set; }
+
+        public bool ShareChannel
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public InvocationInfo MyInvocation
+        {
+            get { return null; }
+        }
+
+        public PSHost Host
+        {
+            get { throw new NotImplementedException(); }
         }
     }
 }
