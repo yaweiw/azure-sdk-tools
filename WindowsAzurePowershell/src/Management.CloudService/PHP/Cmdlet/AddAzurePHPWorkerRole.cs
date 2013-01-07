db08b8e372ad685fed7d2d18c29779361dabce93
ogail <zikas_zak@hotmail.com>
2013-01-06 20:18:21 -0800

    Fix verbose message for Add-AzurePHP*Role

--- WindowsAzurePowershell/src/Management.CloudService/PHP/Cmdlet/AddAzurePHPWorkerRole.cs
+++ WindowsAzurePowershell/src/Management.CloudService/PHP/Cmdlet/AddAzurePHPWorkerRole.cs
@@ -35,7 +35,7 @@ namespace Microsoft.WindowsAzure.Management.CloudService.PHP.Cmdlet
             {
                 service.ChangeRolePermissions(workerRole);
                 SafeWriteOutputPSObject(typeof(RoleSettings).FullName, Parameters.RoleName, workerRole.Name);
-                WriteVerbose(string.Format(Resources.AddRoleMessageCreate, rootPath, workerRole.Name));
+                WriteVerbose(string.Format(Resources.AddRoleMessageCreatePHP, rootPath, workerRole.Name));
             }
             catch (UnauthorizedAccessException)
             {
