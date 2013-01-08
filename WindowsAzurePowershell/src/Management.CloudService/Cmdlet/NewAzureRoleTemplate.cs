adaa543637a14a364fe0694e60330871d001a825
ogail <zikas_zak@hotmail.com>
2013-01-08 12:27:24 -0800

    Implement NewAzureRoleTemplate cmdlet

---
+++ WindowsAzurePowershell/src/Management.CloudService/Cmdlet/NewAzureRoleTemplate.cs
@@ -0,0 +1,64 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Management.CloudService.Cmdlet
+{
+    using System.IO;
+    using System.Management.Automation;
+    using System.Security.Permissions;
+    using Microsoft.WindowsAzure.Management.Cmdlets.Common;
+    using Microsoft.WindowsAzure.Management.Extensions;
+    using Microsoft.WindowsAzure.Management.Utilities;
+    using Microsoft.WindowsAzure.Management.CloudService.Properties;
+
+    /// <summary>
+    /// Creates new azure template for web/worker role.
+    /// </summary>
+    [Cmdlet(VerbsCommon.New, "AzureRoleTemplate")]
+    public class NewAzureRoleTemplateCommand : CmdletBase
+    {
+        const string DefaultWebRoleTemplate = "WebRoleTemplate";
+
+        const string DefaultWorkerRoleTemplate = "WorkerRoleTemplate";
+
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = "WebRole", HelpMessage = "Specifies that the generated template should be for web role")]
+        public SwitchParameter Web { get; set; }
+
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = "WorkerRole", HelpMessage = "Specifies that the generated template should be for worker role")]
+        public SwitchParameter Worker { get; set; }
+
+        [Parameter(Position = 1, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Output path for the generated template")]
+        public string OutputPath { get; set; }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public override void ExecuteCmdlet()
+        {
+            string output = !string.IsNullOrEmpty(OutputPath) ? this.TryResolvePath(OutputPath) :
+                Web.IsPresent ?
+                Path.Combine(CurrentPath(), DefaultWebRoleTemplate) :
+                Path.Combine(CurrentPath(), DefaultWorkerRoleTemplate);
+
+            if (Web.IsPresent)
+            {
+                General.DirectoryCopy(Path.Combine(Resources.GeneralScaffolding, RoleType.WebRole.ToString()), output, true);
+            }
+            else
+            {
+                General.DirectoryCopy(Path.Combine(Resources.GeneralScaffolding, RoleType.WorkerRole.ToString()), output, true);
+            }
+
+            SafeWriteOutputPSObject(null, "Path", output);
+        }
+    }
+}
