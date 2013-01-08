711760c7f589bd0e751338cfad153fa1f0feabd5
ogail <zikas_zak@hotmail.com>
2013-01-08 12:27:24 -0800

    Implement NewAzureRoleTemplate cmdlet

--- WindowsAzurePowershell/src/Management/Utilities/General.cs
+++ WindowsAzurePowershell/src/Management/Utilities/General.cs
@@ -377,5 +377,45 @@ namespace Microsoft.WindowsAzure.Management.Utilities
         {
             return (array == null) ? null : (array.Length == 0) ? string.Empty : array.Skip(1).Aggregate(new StringBuilder(array[0].ToString()), (s, i) => s.Append(delimiter).Append(i), s => s.ToString());
         }
+
+        /// <summary>
+        /// Copies a directory.
+        /// </summary>
+        /// <param name="sourceDirName">The source directory name</param>
+        /// <param name="destDirName">The destination directory name</param>
+        /// <param name="copySubDirs">Should the copy be recursive</param>
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
     }
 }
