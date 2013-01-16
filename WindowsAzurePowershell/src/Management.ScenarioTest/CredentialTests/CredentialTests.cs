bddf488a2f0a3505877570176238d63e20d2a514
ogail <zikas_zak@hotmail.com>
2013-01-15 17:55:17 -0800

    Initial commit

--- WindowsAzurePowershell/src/Management.ScenarioTest/CredentialTests/CredentialTests.cs
+++ WindowsAzurePowershell/src/Management.ScenarioTest/CredentialTests/CredentialTests.cs
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ----------------------------------------------------------------------------------
+
 namespace Management.ScenarioTest.CredentialTests
 {
     using System;
