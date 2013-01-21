08505551027af300dd5fb2206523148e0d24975e
Ismet Kahraman <ismetk@microsoft.com>
2013-01-21 13:49:39 -0800

    Removed 2011 from copy right header.

--- WindowsAzurePowershell/src/Sync/Upload/PatchingBlobCreator.cs
+++ WindowsAzurePowershell/src/Sync/Upload/PatchingBlobCreator.cs
@@ -1,6 +1,6 @@
 // ----------------------------------------------------------------------------------
 //
-// Copyright 2011 Microsoft Corporation
+// Copyright Microsoft Corporation
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -12,14 +12,13 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
-using System.Threading;
-
 namespace Microsoft.WindowsAzure.Sync.Upload
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Threading;
     using Microsoft.WindowsAzure.Storage;
     using Microsoft.WindowsAzure.Storage.Auth;
     using Microsoft.WindowsAzure.Storage.Blob;
