5408b8ec4d2928f0dd07aa87828296fc3d4d4a44
ogail <zikas_zak@hotmail.com>
2013-01-22 14:01:05 -0800

    Add more test categories

---
+++ WindowsAzurePowershell/src/Management.ScenarioTest/Common/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.WindowsAzure.Management.ScenarioTest.Common
+{
+    public class Category
+    {
+        public const string All = "All";
+
+        public const string ServiceBus = "ServiceBus";
+
+        public const string CloudService = "CloudService";
+    }
+}
