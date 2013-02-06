03ba1557d1ae2cc022f2b0dbf6d3c6fbf46e62ce
ogail <zikas_zak@hotmail.com>
2013-02-06 15:08:14 -0800

    Port NewWindowsAzureQuickVM test to ScenarioTest project

--- WindowsAzurePowershell/src/Management.ScenarioTest/Common/Constants.cs
+++ WindowsAzurePowershell/src/Management.ScenarioTest/Common/Constants.cs
@@ -19,5 +19,7 @@ namespace Microsoft.WindowsAzure.Management.ScenarioTest.Common
         public const string Websites = "Websites";
 
         public const string Store = "Store";
+
+        public const string ServiceManagemenet = "ServiceManagemenet";
     }
 }
