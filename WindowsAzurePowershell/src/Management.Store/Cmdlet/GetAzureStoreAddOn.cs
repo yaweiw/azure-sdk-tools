174ac9be32b47572506b3ea3ac00ae4c1af29935
ogail <Abdelrahman.Elogeel@microsoft.com>
2013-02-21 14:08:54 -0800

    Merge GetAzureStoreAvailableAddOn int GetAzureStoreAddOn

--- WindowsAzurePowershell/src/Management.Store/Cmdlet/GetAzureStoreAddOn.cs
+++ WindowsAzurePowershell/src/Management.Store/Cmdlet/GetAzureStoreAddOn.cs
@@ -15,38 +15,71 @@
 namespace Microsoft.WindowsAzure.Management.Store.Cmdlet
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Management.Automation;
     using System.Security.Permissions;
     using Microsoft.Samples.WindowsAzure.ServiceManagement;
     using Microsoft.WindowsAzure.Management.Cmdlets.Common;
     using Microsoft.WindowsAzure.Management.Store.Model;
+    using Microsoft.WindowsAzure.Management.Store.Properties;
 
     /// <summary>
-    /// Gets all purchased Add-Ons or specific Add-On
+    /// Gets all available Windows Azure add-ons from Marketplace and gets user purchased add-ons.
     /// </summary>
-    [Cmdlet(VerbsCommon.Get, "AzureStoreAddOn"), OutputType(typeof(List<PSObject>))]
+    [Cmdlet(VerbsCommon.Get, "AzureStoreAddOn"), OutputType(typeof(List<PSObject>), typeof(List<WindowsAzureOffer>))]
     public class GetAzureStoreAddOnCommand : CloudBaseCmdlet<IServiceManagement>
     {
         const string ShortType = "Microsoft.WindowsAzure.Management.Store.Model.AddOnWindowsAzureAddOn.Short";
 
+        const string ListAvailableParameterSet = "ListAvailable";
+
+        const string GetAddOnParameterSet = "GetAddOn";
+
         public StoreClient StoreClient { get; set; }
 
-        [Parameter(Position = 0, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Add-On name")]
+        public MarketplaceClient MarketplaceClient { get; set; }
+
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipelineByPropertyName = true, 
+            ParameterSetName = ListAvailableParameterSet, HelpMessage = "List available add-ons")]
+        public SwitchParameter ListAvailable { get; set; }
+
+        [Parameter(Position = 1, Mandatory = false, ValueFromPipelineByPropertyName = true, 
+            ParameterSetName = ListAvailableParameterSet, HelpMessage = "Country code")]
+        [ValidateCountryLengthAttribute()]
+        public string Country { get; set; }
+
+        [Parameter(Position = 0, Mandatory = false, ValueFromPipelineByPropertyName = true, 
+            ParameterSetName = GetAddOnParameterSet, HelpMessage = "Add-On name")]
         public string Name { get; set; }
 
-        [Parameter(Position = 1, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Add-On provider")]
+        [Parameter(Position = 1, Mandatory = false, ValueFromPipelineByPropertyName = true, 
+            ParameterSetName = GetAddOnParameterSet, HelpMessage = "Add-On provider")]
         public string Provider { get; set; }
 
-        [Parameter(Position = 2, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Add-On location")]
+        [Parameter(Position = 2, Mandatory = false, ValueFromPipelineByPropertyName = true, 
+            ParameterSetName = GetAddOnParameterSet, HelpMessage = "Add-On location")]
         public string Location { get; set; }
 
-        [Parameter(Position = 3, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "Show add-on details")]
+        [Parameter(Position = 3, Mandatory = false, ValueFromPipelineByPropertyName = true, 
+            ParameterSetName = GetAddOnParameterSet, HelpMessage = "Show add-on details")]
         public SwitchParameter Detailed { get; set; }
 
         [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
         public override void ExecuteCmdlet()
         {
-            StoreClient = StoreClient ?? new StoreClient(
+            if (ListAvailable.IsPresent)
+	        {
+		        ListAvailableAddOns();
+	        }
+            else
+	        {
+                GetAddOn();
+	        }
+        }
+
+        private void GetAddOn()
+        {
+ 	        StoreClient = StoreClient ?? new StoreClient(
                 CurrentSubscription.SubscriptionId,
                 ServiceEndpoint,
                 CurrentSubscription.Certificate,
@@ -70,6 +103,19 @@ namespace Microsoft.WindowsAzure.Management.Store.Cmdlet
                 });
                 WriteObject(shortObjects, true);
             }
+        }
+
+        private void ListAvailableAddOns()
+        {
+ 	        LocationList locations = Channel.ListLocations(CurrentSubscription.SubscriptionId);
+            MarketplaceClient = MarketplaceClient ?? 
+                new MarketplaceClient(locations.Select<Location, string>(l => l.Name));
+
+            WriteVerbose(Resources.GetAllAddOnsWaitMessage);
+            List<WindowsAzureOffer> result = MarketplaceClient.GetAvailableWindowsAzureOffers(Country);
+            List<WindowsAzureOffer> knownProviders = result.Where<WindowsAzureOffer>(
+                o => MarketplaceClient.IsKnownProvider(o.ProviderId)).ToList<WindowsAzureOffer>();
+            WriteObject(knownProviders, true);
         }
     }
 }
