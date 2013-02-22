44452cb96ed1e05901a0db0029618ed743a28d0d
Ismet Kahraman <ismetk@microsoft.com>
2013-02-21 16:42:15 -0800

    Feature work: -Enhance Virtual Machine Imaging Support (#913) -Add Simultaneous Upgrade Mode (#910)

--- WindowsAzurePowershell/src/Management.ServiceManagement/IaaS/DiskRepository/AddAzureVMImage.cs
+++ WindowsAzurePowershell/src/Management.ServiceManagement/IaaS/DiskRepository/AddAzureVMImage.cs
@@ -12,15 +12,14 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
-using Microsoft.WindowsAzure.ServiceManagement;
-
 namespace Microsoft.WindowsAzure.Management.ServiceManagement.IaaS.DiskRepository
 {
     using System;
     using System.Management.Automation;
     using Model;
     using Cmdlets.Common;
-    using Extensions;
+    using WindowsAzure.ServiceManagement;
+
 
     [Cmdlet(VerbsCommon.Add, "AzureVMImage"), OutputType(typeof(OSImageContext))]
     public class AddAzureVMImage : ServiceManagementBaseCmdlet
@@ -49,9 +48,57 @@ namespace Microsoft.WindowsAzure.Management.ServiceManagement.IaaS.DiskRepositor
             set;
         }
 
-        [Parameter(ValueFromPipelineByPropertyName = true, HelpMessage = "Label of the image.")]
+        [Parameter(Position = 3, ValueFromPipelineByPropertyName = true, HelpMessage = "Label of the image.")]
         [ValidateNotNullOrEmpty]
         public string Label
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 4, ValueFromPipelineByPropertyName = true, HelpMessage = "Specifies the End User License Aggreement, recommended value is a URL.")]
+        [ValidateNotNullOrEmpty]
+        public string Eula
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 5, ValueFromPipelineByPropertyName = true, HelpMessage = "Specifies the description of the OS image.")]
+        [ValidateNotNullOrEmpty]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 6, ValueFromPipelineByPropertyName = true, HelpMessage = "Specifies a value that can be used to group OS images.")]
+        [ValidateNotNullOrEmpty]
+        public string ImageFamily
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 7, ValueFromPipelineByPropertyName = true, HelpMessage = "Specifies the date when the OS image was added to the image repository.")]
+        [ValidateNotNullOrEmpty]
+        public DateTime? PublishedDate
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 8, ValueFromPipelineByPropertyName = true, HelpMessage = "Specifies the URI that points to a document that contains the privacy policy related to the OS image.")]
+        [ValidateNotNullOrEmpty]
+        public Uri PrivacyUri
+        {
+            get;
+            set;
+        }
+        [Parameter(Position = 9, ValueFromPipelineByPropertyName = true, HelpMessage = " Specifies the size to use for the virtual machine that is created from the OS image.")]
+        [ValidateNotNullOrEmpty]
+        [ValidateSet(RecommendedVMSizeType.Small, RecommendedVMSizeType.Medium, RecommendedVMSizeType.Large, RecommendedVMSizeType.ExtraLarge)]
+        public string RecommendedVMSize
         {
             get;
             set;
@@ -64,7 +111,13 @@ namespace Microsoft.WindowsAzure.Management.ServiceManagement.IaaS.DiskRepositor
                 Name = this.ImageName,
                 MediaLink = new Uri(this.MediaLocation),
                 Label = string.IsNullOrEmpty(this.Label) ? this.ImageName : this.Label,
-                OS = this.OS
+                OS = this.OS,
+                Eula = this.Eula,
+                Description = this.Description,
+                ImageFamily = this.ImageFamily,
+                PublishedDate = this.PublishedDate,
+                PrivacyUri = this.PrivacyUri,
+                RecommendedVMSize = this.RecommendedVMSize
             };
 
             ExecuteClientActionInOCS(
@@ -81,6 +134,15 @@ namespace Microsoft.WindowsAzure.Management.ServiceManagement.IaaS.DiskRepositor
                     MediaLink = responseImage.MediaLink,
                     ImageName = responseImage.Name,
                     OS = responseImage.OS,
+                    LogicalSizeInGB = responseImage.LogicalSizeInGB,
+                    Eula = responseImage.Eula,
+                    Description = responseImage.Description,
+                    ImageFamily = responseImage.ImageFamily,
+                    PublishedDate = responseImage.PublishedDate,
+                    IsPremium =  responseImage.IsPremium,
+                    PrivacyUri = responseImage.PrivacyUri,
+                    PublisherName = responseImage.PublisherName,
+                    RecommendedVMSize = responseImage.RecommendedVMSize,
                     OperationDescription = CommandRuntime.ToString(),
                     OperationId = op.OperationTrackingId,
                     OperationStatus = op.Status
