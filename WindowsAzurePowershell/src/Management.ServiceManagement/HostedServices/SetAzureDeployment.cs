f1016764151f5dd3d30b139b24828ce81b466ed7
Ismet Kahraman <ismetk@microsoft.com>
2013-02-21 16:42:15 -0800

    Feature work: -Enhance Virtual Machine Imaging Support (#913) -Add Simultaneous Upgrade Mode (#910)

--- WindowsAzurePowershell/src/Management.ServiceManagement/HostedServices/SetAzureDeployment.cs
+++ WindowsAzurePowershell/src/Management.ServiceManagement/HostedServices/SetAzureDeployment.cs
@@ -98,8 +98,8 @@ namespace Microsoft.WindowsAzure.Management.ServiceManagement.HostedServices
             set;
         }
 
-        [Parameter(Position = 5, ParameterSetName = "Upgrade", HelpMessage = "Upgrade mode. Auto | Manual")]
-        [ValidateSet(UpgradeType.Auto, UpgradeType.Manual)]
+        [Parameter(Position = 5, ParameterSetName = "Upgrade", HelpMessage = "Upgrade mode. Auto | Manual | Simultaneous")]
+        [ValidateSet(UpgradeType.Auto, UpgradeType.Manual, UpgradeType.Simultaneous)]
         public string Mode
         {
             get;
