d698e9c7f655545fb2170c305bd84e7f1a1bdb2c
Yao <yaogao@microsoft.com>
2013-03-01 14:08:03 +0800

    fix typo

--- WindowsAzurePowershell/src/Management.Storage/Common/StorageCloudCmdletBase.cs
+++ WindowsAzurePowershell/src/Management.Storage/Common/StorageCloudCmdletBase.cs
@@ -255,7 +255,7 @@ namespace Microsoft.WindowsAzure.Management.Storage.Common
         {
             double timespan = CmdletOperationContext.GetRunningMilliseconds();
             string message = string.Format(Resources.EndProcessingLog,
-                this.GetType().Name, CmdletOperationContext.StartedRemoteCallCounter, CmdletOperationContext.FinisedhRemoteCallCounter, timespan, CmdletOperationContext.ClientRequestId);
+                this.GetType().Name, CmdletOperationContext.StartedRemoteCallCounter, CmdletOperationContext.FinishedRemoteCallCounter, timespan, CmdletOperationContext.ClientRequestId);
             WriteVerboseLog(message);
             base.EndProcessing();
         }
