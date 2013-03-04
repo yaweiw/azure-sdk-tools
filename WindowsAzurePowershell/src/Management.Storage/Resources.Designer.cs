ad736bb70149112303d539e745792769f6a32885
Yao <yaogao@microsoft.com>
2013-03-04 15:58:38 +0800

    fix typo errors

--- WindowsAzurePowershell/src/Management.Storage/Resources.Designer.cs
+++ WindowsAzurePowershell/src/Management.Storage/Resources.Designer.cs
@@ -160,7 +160,7 @@ namespace Microsoft.WindowsAzure.Management.Storage {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The blob &apos;{0}&apos; in container &apos;{1}&apos; has snapshot. Are you sure to remove blob and its snapshots?.
+        ///   Looks up a localized string similar to The blob &apos;{0}&apos; in container &apos;{1}&apos; has snapshots. Are you sure to remove blob and its snapshots?.
         /// </summary>
         internal static string ConfirmRemoveBlobWithSnapshot {
             get {
@@ -576,9 +576,9 @@ namespace Microsoft.WindowsAzure.Management.Storage {
         /// <summary>
         ///   Looks up a localized string similar to The remove operation of  blob &apos;{0}&apos; in container &apos;{1}&apos; is cancelled..
         /// </summary>
-        internal static string RemoveBlobCanncelled {
+        internal static string RemoveBlobCancelled {
             get {
-                return ResourceManager.GetString("RemoveBlobCanncelled", resourceCulture);
+                return ResourceManager.GetString("RemoveBlobCancelled", resourceCulture);
             }
         }
         
