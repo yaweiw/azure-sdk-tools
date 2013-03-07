b0682010ab5343bd25530245573003c4c7c6e7b6
Yao <yaogao@microsoft.com>
2013-03-07 20:23:50 +0800

    add http status code for debug purpose and add copy right

--- WindowsAzurePowershell/src/Management.Storage/Blob/Cmdlet/GetAzureStorageBlobCopyState.cs
+++ WindowsAzurePowershell/src/Management.Storage/Blob/Cmdlet/GetAzureStorageBlobCopyState.cs
@@ -1,16 +1,28 @@
-﻿using Microsoft.WindowsAzure.Management.Storage.Common;
-using Microsoft.WindowsAzure.ServiceManagement.Storage.Blob.ResourceModel;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Management.Automation;
-using System.Text;
-using System.Threading;
+﻿﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
 
 namespace Microsoft.WindowsAzure.Management.Storage.Blob.Cmdlet
 {
+    using Microsoft.WindowsAzure.Management.Storage.Common;
+    using Microsoft.WindowsAzure.ServiceManagement.Storage.Blob.ResourceModel;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using System;
+    using System.Collections.Generic;
+    using System.Management.Automation;
+    using System.Threading;
+
     [Cmdlet(VerbsCommon.Get, StorageNouns.CopyBlobStatus, DefaultParameterSetName = NameParameterSet),
        OutputType(typeof(AzureStorageBlob))]
     public class GetAzureStorageBlobCopyState : StorageCloudBlobCmdletBase
