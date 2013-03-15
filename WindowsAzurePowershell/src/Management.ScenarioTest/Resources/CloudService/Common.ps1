3d055a834f0ee1ca33bcb617fe49c52b66b1a190
ogail <Abdelrahman.Elogeel@microsoft.com>
2013-03-14 23:01:03 -0700

    Fix scenario test failures

--- WindowsAzurePowershell/src/Management.ScenarioTest/Resources/CloudService/Common.ps1
+++ WindowsAzurePowershell/src/Management.ScenarioTest/Resources/CloudService/Common.ps1
@@ -47,14 +47,15 @@ The number of cloud services to create.
 #>
 function New-CloudService
 {
-	param([int] $count, [ScriptBlock] $cloudServiceProject)
+	param([int] $count, [ScriptBlock] $cloudServiceProject, [string] $slot)
 	
 	if ($cloudServiceProject -eq $null) { $cloudServiceProject = { New-TinyCloudServiceProject $args[0] } }
+	if ($slot -eq $null) { $slot = "Production" }
 
 	1..$count | % { 
 		$name = Get-CloudServiceName;
 		Invoke-Command -ScriptBlock $cloudServiceProject -ArgumentList $name;
-		Publish-AzureServiceProject -Force;
+		Publish-AzureServiceProject -Force -Slot $slot
 		$global:createdCloudServices += $name;
 	}
 }
@@ -65,8 +66,8 @@ Removes all cloud services/storage accounts in the current subscription.
 #>
 function Initialize-CloudServiceTest
 {
-	<# To Do: implement when we have unsigned version from Management.ServiceManagement assembly #>
-	$global:createdCloudServices = @()
+	Get-AzureStorageAccount | Remove-AzureStorageAccount
+	Get-AzureService | Remove-AzureService -Force
 }
 
 <#
