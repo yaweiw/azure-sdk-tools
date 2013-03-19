60c929f976d1153b64fcc32ba1b6e9aafb9ddaec
ogail <Abdelrahman.Elogeel@microsoft.com>
2013-03-18 17:56:18 -0700

    Implement ListPath switch

--- WindowsAzurePowershell/src/Management.WebSites/Services/IDeploymentServiceManagement.cs
+++ WindowsAzurePowershell/src/Management.WebSites/Services/IDeploymentServiceManagement.cs
@@ -63,6 +63,6 @@ namespace Microsoft.WindowsAzure.Management.Websites.Services
         [OperationContract(AsyncPattern = true)]
         [WebInvoke(Method = "GET", UriTemplate = "vfs/LogFiles")]
         IAsyncResult BeginListPaths(AsyncCallback callback, object state);
-        string EndListPaths(IAsyncResult asyncResult);
+        List<Item> EndListPaths(IAsyncResult asyncResult);
     }
 }
