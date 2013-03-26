06c740556f1c12c4b7396e3cdd263ed91a03c744
ogail <Abdelrahman.Elogeel@microsoft.com>
2013-03-26 11:35:31 -0700

    Set AssemblyVersion to 0.6.12

--- WindowsAzurePowershell/src/ServiceManagement.Additions/Properties/AssemblyInfo.cs
+++ WindowsAzurePowershell/src/ServiceManagement.Additions/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.12")]
 [assembly: AssemblyFileVersion("0.6.12")]
