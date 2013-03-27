06addf3d2b35f49815fa6efae58605232f988f8a
ogail <Abdelrahman.Elogeel@microsoft.com>
2013-03-26 20:31:09 -0700

    Combine Websites project into Management

--- WindowsAzurePowershell/src/Management.Utilities/Websites/Services/LinkedRevisionControl.cs
+++ WindowsAzurePowershell/src/Management.Utilities/Websites/Services/LinkedRevisionControl.cs
@@ -38,7 +38,7 @@ namespace Microsoft.WindowsAzure.Management.Utilities.Websites.Services
             {
                 // Scaffold gitignore
                 string cmdletPath = Directory.GetParent(InvocationPath).FullName;
-                File.Copy(Path.Combine(cmdletPath, "Scaffolding/Node/.gitignore"), ".gitignore");
+                File.Copy(Path.Combine(cmdletPath, "Scaffolding/Node/Website/.gitignore"), ".gitignore");
             }
         }
 
