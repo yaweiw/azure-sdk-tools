5ef25569644dd8781f530c16cd9f66a707c9db0b
Yao <yaogao@microsoft.com>
2013-04-03 11:51:34 +0800

    add queue cmdlet and unit tests

---
+++ WindowsAzurePowershell/src/Management.Storage/Model/Contract/IStorageQueueManagement.cs
@@ -0,0 +1,36 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ---------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Management.Storage.Model.Contract
+{
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Queue;
+    using Microsoft.WindowsAzure.Storage.Queue.Protocol;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public interface IStorageQueueManagement
+    {
+        IEnumerable<CloudQueue>  ListQueues(string prefix, QueueListingDetails queueListingDetails, QueueRequestOptions options, OperationContext operationContext);
+        void FetchAttributes(CloudQueue queue, QueueRequestOptions options, OperationContext operationContext);
+        CloudQueue GetQueueReference(String name);
+
+        bool IsQueueExists(CloudQueue queue, QueueRequestOptions requestOptions, OperationContext operationContext);
+
+        bool CreateQueueIfNotExists(CloudQueue queue, QueueRequestOptions options, OperationContext operationContext);
+        void DeleteQueue(CloudQueue queue, QueueRequestOptions options, OperationContext operationContext);
+    }
+}
