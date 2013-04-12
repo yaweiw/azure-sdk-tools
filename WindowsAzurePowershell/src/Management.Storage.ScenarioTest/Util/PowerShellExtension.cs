d3362ce3904d420cff24d57a7135afb14a2d4e06
Yao <yaogao@microsoft.com>
2013-04-12 17:59:17 +0800

    remove redundant parameter check

---
+++ WindowsAzurePowershell/src/Management.Storage.ScenarioTest/Util/PowerShellExtension.cs
@@ -0,0 +1,54 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace CLITest.Util
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Text;
+
+    public static class PowerShellExtension
+    {
+        public static void BindParameter(this PowerShell ps, string parameter, string value)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                ps.AddParameter(parameter, value);
+            }
+        }
+
+        public static void BindParameter(this PowerShell ps, string parameter, bool value)
+        {
+            if (value)
+            {
+                ps.AddParameter(parameter);
+            }
+        }
+
+        public static void BindParameter(this PowerShell ps, string parameter, object value)
+        {
+            if (value != null)
+            {
+                ps.AddParameter(parameter, value);
+            }
+        }
+
+        public static void BindParameter(this PowerShell ps, string parameter)
+        {
+            ps.AddParameter(parameter);
+        }
+    }
+}
