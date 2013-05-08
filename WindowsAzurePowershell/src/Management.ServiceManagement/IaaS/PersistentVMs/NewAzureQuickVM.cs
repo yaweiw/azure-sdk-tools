f75353828037066094452e5ab23ebcba241c9ca0
ogail <Abdelrahman.Elogeel@microsoft.com>
2013-05-08 11:04:38 -0700

    Change storage account fetch using on the fly channel

--- WindowsAzurePowershell/src/Management.ServiceManagement/IaaS/PersistentVMs/NewAzureQuickVM.cs
+++ WindowsAzurePowershell/src/Management.ServiceManagement/IaaS/PersistentVMs/NewAzureQuickVM.cs
@@ -257,7 +257,7 @@ namespace Microsoft.WindowsAzure.Management.ServiceManagement.IaaS.PersistentVMs
             CloudStorageAccount currentStorage = null;
             try
             {
-                currentStorage = currentSubscription.GetCurrentStorageAccount(Channel);
+                currentStorage = currentSubscription.GetCurrentStorageAccount();
             }
             catch (ServiceManagementClientException) // couldn't access
             {
