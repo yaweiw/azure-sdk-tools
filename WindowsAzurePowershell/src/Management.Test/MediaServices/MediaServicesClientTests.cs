0eae937f176f0d775a2637d4b89d6077be689d2b
Ivan Shaporov <ivan_shaporov@yahoo.com>
2013-08-16 16:23:15 -0700

    More unit tests.

--- WindowsAzurePowershell/src/Management.Test/MediaServices/MediaServicesClientTests.cs
+++ WindowsAzurePowershell/src/Management.Test/MediaServices/MediaServicesClientTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.WindowsAzure.Management.Utilities.Common;
 using Microsoft.WindowsAzure.Management.Utilities.MediaService;
+using Microsoft.WindowsAzure.Management.Utilities.MediaService.Services.MediaServicesEntities;
 using Microsoft.WindowsAzure.ServiceManagement;
 
 namespace Microsoft.WindowsAzure.Management.Test.MediaServices
@@ -120,7 +121,6 @@ namespace Microsoft.WindowsAzure.Management.Test.MediaServices
 
             string responseText = "[{\"Name\":\"testps\",\"Type\":\"MediaService\",\"State\":\"Active\",\"AccountId\":\"E0658294-5C96-4B0F-AD55-F7446CE4F788\"},{\"Name\":\"test2\",\"Type\":\"MediaService\",\"State\":\"Active\",\"AccountId\":\"C92B17C8-5422-4CD1-8D3C-61E576E861DD\"}]";
 
-
             var response = new HttpResponseMessage(HttpStatusCode.OK)
             {
                 Content = new FakeHttpContent(responseText)
@@ -139,6 +139,137 @@ namespace Microsoft.WindowsAzure.Management.Test.MediaServices
             var result = target.GetMediaServiceAccountsAsync().Result.ToArray();
             Assert.AreEqual(Guid.Parse("E0658294-5C96-4B0F-AD55-F7446CE4F788"), result[0].AccountId);
             Assert.AreEqual(Guid.Parse("C92B17C8-5422-4CD1-8D3C-61E576E861DD"), result[1].AccountId);
+        }
+
+        [TestMethod]
+        public void TestGetStorageServiceKeys()
+        {
+            var fakeHttpHandler = new FakeHttpMessageHandler();
+
+            string responseText = "<StorageService xmlns=\"http://schemas.microsoft.com/windowsazure\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\"><Url>https://management.core.windows.net/f7190519-c29e-47f2-9019-c5a94c8e75f9/services/storageservices/nimbusivshapo</Url><StorageServiceKeys><Primary>PrimaryKey</Primary><Secondary>SecondaryKey</Secondary></StorageServiceKeys></StorageService>";
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new FakeHttpContent(responseText)
+            };
+
+            fakeHttpHandler.Send = request => response;
+
+            var fakeHttpClient = fakeHttpHandler.CreateIMediaServicesHttpClient();
+
+            var target = new MediaServicesClient(
+                new SubscriptionData { SubscriptionId = _subscriptionId },
+                null,
+                fakeHttpClient,
+                fakeHttpClient);
+
+            var result = target.GetStorageServiceKeys(_accountName).Result;
+            Assert.AreEqual("PrimaryKey", result.StorageServiceKeys.Primary);
+            Assert.AreEqual("SecondaryKey", result.StorageServiceKeys.Secondary);
+        }
+
+        [TestMethod]
+        public void TestGetStorageServiceKeysInvalidAccountName()
+        {
+            var fakeHttpHandler = new FakeHttpMessageHandler();
+
+            string responseText = "<Error xmlns=\"http://schemas.microsoft.com/windowsazure\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\"><Code>BadRequest</Code><Message>The name is not a valid.</Message></Error>";
+
+            var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new FakeHttpContent(responseText)
+            };
+
+            fakeHttpHandler.Send = request => response;
+
+            var fakeHttpClient = fakeHttpHandler.CreateIMediaServicesHttpClient();
+
+            var target = new MediaServicesClient(
+                new SubscriptionData { SubscriptionId = _subscriptionId },
+                null,
+                fakeHttpClient,
+                fakeHttpClient);
+
+            try
+            {
+                var result = target.GetStorageServiceKeys(_accountName).Result;
+            }
+            catch (AggregateException ax)
+            {
+                ServiceManagementClientException x = (ServiceManagementClientException)ax.InnerExceptions.Single();
+
+                Assert.AreEqual(HttpStatusCode.BadRequest, x.HttpStatus);
+                return;
+            }
+
+            Assert.Fail("ServiceManagementClientException expected");
+        }
+
+        [TestMethod]
+        public void TestCreateNewAzureMediaServiceAsync()
+        {
+            var fakeHttpHandler = new FakeHttpMessageHandler();
+
+            string responseText = "{\"AccountId\":\"abe5afa0-704b-4d07-b5d8-5b0b039474e7\",\"AccountName\":\"tmp\",\"StatusCode\":201,\"Subscription\":\"f7190519-c29e-47f2-9019-c5a94c8e75f9\"}";
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new FakeHttpContent(responseText)
+            };
+
+            fakeHttpHandler.Send = request => response;
+
+            var fakeHttpClient = fakeHttpHandler.CreateIMediaServicesHttpClient();
+
+            var target = new MediaServicesClient(
+                new SubscriptionData { SubscriptionId = _subscriptionId },
+                null,
+                fakeHttpClient,
+                fakeHttpClient);
+
+            var creationRequest = new AccountCreationRequest { AccountName = _accountName };
+
+            var result = target.CreateNewAzureMediaServiceAsync(creationRequest).Result;
+            Assert.AreEqual("tmp", result.Name);
+        }
+
+        [TestMethod]
+        public void TestCreateNewAzureMediaServiceAsyncInvalidAccount()
+        {
+            var fakeHttpHandler = new FakeHttpMessageHandler();
+
+            string responseText = "<string xmlns=\"http://schemas.microsoft.com/2003/10/Serialization/\">{\"Code\":\"BadRequest\",\"Message\":\"Account Creation Request contains an invalid account name.\"}</string>";
+
+            var response = new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new FakeHttpContent(responseText)
+            };
+
+            fakeHttpHandler.Send = request => response;
+
+            var fakeHttpClient = fakeHttpHandler.CreateIMediaServicesHttpClient();
+
+            var target = new MediaServicesClient(
+                new SubscriptionData { SubscriptionId = _subscriptionId },
+                null,
+                fakeHttpClient,
+                fakeHttpClient);
+
+            var creationRequest = new AccountCreationRequest { AccountName = _accountName };
+
+            try
+            {
+                var result = target.CreateNewAzureMediaServiceAsync(creationRequest).Result;
+            }
+            catch (AggregateException ax)
+            {
+                ServiceManagementClientException x = (ServiceManagementClientException)ax.InnerExceptions.Single();
+
+                Assert.AreEqual(HttpStatusCode.BadRequest, x.HttpStatus);
+                return;
+            }
+
+            Assert.Fail("ServiceManagementClientException expected");
         }
     }
 }
---
+++ WindowsAzurePowershell/src/Management.Test/MediaServices/NewAzureMediaServiceTests.cs
@@ -0,0 +1,77 @@
+﻿// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.WindowsAzure.Management.MediaService;
+using Microsoft.WindowsAzure.Management.Utilities.MediaService;
+using Microsoft.WindowsAzure.Management.Utilities.MediaService.Services.MediaServicesEntities;
+using Moq;
+
+namespace Microsoft.WindowsAzure.Management.Test.MediaServices
+{
+    using System.Linq;
+    using Utilities.Common;
+    using Utilities.Websites;
+    using Management.Utilities.Common;
+    using VisualStudio.TestTools.UnitTesting;
+    using System.Threading.Tasks;
+    using System;
+    using Microsoft.WindowsAzure.ServiceManagement;
+
+    [TestClass]
+    public class NewAzureMediaServiceTests : TestBase
+    {
+        [TestMethod]
+        public void NewAzureMediaServiceTest()
+        {
+            var clientMock = new Mock<IMediaServicesClient>();
+
+            string expectedName = "testacc";
+
+            var fakeResult = new AccountCreationResult
+            {
+                Name = expectedName,
+            };
+
+            var command = new NewAzureMediaServiceCommand()
+            {
+                CommandRuntime = new MockCommandRuntime(),
+                Name = expectedName,
+                MediaServicesClient = clientMock.Object,
+                StorageAccountKey = "testKey",
+                StorageAccountName = "storageaccount",
+                Location = "wus"
+            };
+
+            clientMock.Setup(f => f.GetStorageServiceProperties(command.StorageAccountName)).Returns(Task.Factory.StartNew(() =>
+            {
+                var properties = new StorageServiceProperties { Endpoints = new EndpointList() };
+                properties.Endpoints.Add("http://endpoint");
+
+                return new StorageService
+                {
+                    StorageServiceProperties = properties
+                };
+            }));
+
+            clientMock.Setup(f => f.CreateNewAzureMediaServiceAsync(It.IsAny<AccountCreationRequest>())).Returns(Task.Factory.StartNew(() =>
+            {
+                return fakeResult;
+            }));
+
+            // Test
+            command.ExecuteCmdlet();
+            Assert.AreEqual(1, ((MockCommandRuntime)command.CommandRuntime).OutputPipeline.Count);
+            var result = (AccountCreationResult)((MockCommandRuntime)command.CommandRuntime).OutputPipeline.FirstOrDefault();
+            Assert.AreEqual(expectedName, result.Name);
+        }
+    }
+}
