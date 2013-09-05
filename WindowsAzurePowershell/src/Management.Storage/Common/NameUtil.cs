23d1d5c0b2d9e730e5f54a2234711c258e2052d7
Yao <yaogao@microsoft.com>
2013-09-05 16:48:16 +0800

    work with sastoken in connection string and fix typo

--- WindowsAzurePowershell/src/Management.Storage/Common/NameUtil.cs
+++ WindowsAzurePowershell/src/Management.Storage/Common/NameUtil.cs
@@ -116,8 +116,16 @@ namespace Microsoft.WindowsAzure.Management.Storage.Common
         /// <returns>True for valid table name, otherwise return false</returns>
         public static bool IsValidTableName(string tableName)
         {
-            Regex regex = new Regex(@"^[A-Za-z][A-Za-z0-9]{2,62}$");
-            return regex.IsMatch(tableName);
+            //http://msdn.microsoft.com/en-us/library/windowsazure/hh343258.aspx
+            string [] metricsTables = {"$MetricsTransactionsBlob", "$MetricsTransactionsTable",
+                                          "$MetricsTransactionsQueue",  "$MetricsCapacityBlob"};
+            if (!String.IsNullOrEmpty(tableName) && Array.IndexOf(metricsTables, tableName) != -1)
+            {
+                return true;
+            } else {
+                Regex regex = new Regex(@"^[A-Za-z][A-Za-z0-9]{2,62}$");
+                return regex.IsMatch(tableName);
+            }
         }
 
         /// <summary>
--- WindowsAzurePowershell/src/Management.Storage/Model/ResourceModel/AzureStorageContext.cs
+++ WindowsAzurePowershell/src/Management.Storage/Model/ResourceModel/AzureStorageContext.cs
@@ -64,9 +64,22 @@ namespace Microsoft.WindowsAzure.Management.Storage.Model.ResourceModel
         public AzureStorageContext(CloudStorageAccount account)
         {
             StorageAccount = account;
-            BlobEndPoint = account.BlobEndpoint.ToString();
-            TableEndPoint = account.TableEndpoint.ToString();
-            QueueEndPoint = account.QueueEndpoint.ToString();
+
+            if (account.BlobEndpoint != null)
+            {
+                BlobEndPoint = account.BlobEndpoint.ToString();
+            }
+
+            if (account.TableEndpoint != null)
+            {
+                TableEndPoint = account.TableEndpoint.ToString();
+            }
+
+            if (account.QueueEndpoint != null)
+            {
+                QueueEndPoint = account.QueueEndpoint.ToString();
+            }
+
             StorageAccountName = account.Credentials.AccountName;
             Context = this;
             Name = String.Empty;
