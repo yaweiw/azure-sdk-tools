1ec0a78b70f4f936c1d28ee9ff7d235da74ef6af
Chris Tavares <ctavares@microsoft.com>
2013-10-04 17:37:37 -0700

    Pass environment to the GetToken methods.

--- WindowsAzurePowershell/src/Commands.Utilities/Common/Authentication/ITokenProvider.cs
+++ WindowsAzurePowershell/src/Commands.Utilities/Common/Authentication/ITokenProvider.cs
@@ -23,17 +23,19 @@ namespace Microsoft.WindowsAzure.Commands.Utilities.Common.Authentication
         /// <summary>
         /// Get a token for the given userID and login type.
         /// </summary>
+        /// <param name="environment">The environment to request a token for.</param>
         /// <param name="userId">ID of user to retrieve token for. If null,
         /// requests a whole new token, prompting user for credentials.</param>
         /// <param name="loginType">What kind of login user is doing.</param>
         /// <returns>An access token or null if authentication is canceled.</returns>
-        IAccessToken GetToken(string userId, LoginType loginType);
+        IAccessToken GetToken(WindowsAzureEnvironment environment, string userId, LoginType loginType);
 
         /// <summary>
         /// Get a new login token, prompting user for credentials.
         /// </summary>
-        /// <param name="loginType"></param>
-        /// <returns></returns>
-        IAccessToken GetNewToken(LoginType loginType);
+        /// <param name="environment">Environment to request a token for.</param>
+        /// <param name="loginType">What kind of login the user is doing.</param>
+        /// <returns>An access token or null if authentication is canceled.</returns>
+        IAccessToken GetNewToken(WindowsAzureEnvironment environment, LoginType loginType);
     }
 }
