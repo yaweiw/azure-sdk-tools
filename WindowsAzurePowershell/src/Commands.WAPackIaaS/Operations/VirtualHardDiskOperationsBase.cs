dec9e77b8f836a6dc61071dcb6aefcf28c99e706
spclark <spclark@microsoft.com>
2013-10-07 17:12:03 -0700

    Backend stubs

---
+++ WindowsAzurePowershell/src/Commands.WAPackIaaS/Operations/VirtualHardDiskOperationsBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.WindowsAzure.Commands.WAPackIaaS.Operations
+{
+    using DataContract;
+    using System;
+    using System.Collections.Generic;
+
+    public class VirtualHardDiskOperationsBase : OperationsBase<VirtualHardDisk>
+    {
+        public override VirtualHardDisk Read(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<VirtualHardDisk> Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WindowsAzurePowershell/src/Commands.WAPackIaaS/Operations/VirtualMachineOperationsBase.cs
@@ -0,0 +1,39 @@
+﻿namespace Microsoft.WindowsAzure.Commands.WAPackIaaS.Operations
+{
+    using DataContract;
+    using System;
+    using System.Collections.Generic;
+
+    public class VirtualMachineOperationsBase : OperationsBase<VirtualMachine>
+    {
+        public override VirtualMachine Create(VirtualMachine toCreate, out Guid jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override VirtualMachine Read(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<VirtualMachine> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<VirtualMachine> Read(string vmName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override VirtualMachine Update(VirtualMachine toUpdate, out Guid jobId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete(Guid id, out Guid jobId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
