19bc06cce4d6e5f33d5bbe4296f2dcacf0338261
spclark <spclark@microsoft.com>
2013-10-07 17:12:03 -0700

    Backend stubs

---
+++ WindowsAzurePowershell/src/Commands.WAPackIaaS/Operations/CloudOperationsBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.WindowsAzure.Commands.WAPackIaaS.Operations
+{
+    using DataContract;
+    using System;
+    using System.Collections.Generic;
+
+    public class CloudOperationsBase : OperationsBase<Cloud>
+    {
+        public override Cloud Read(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Cloud> Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WindowsAzurePowershell/src/Commands.WAPackIaaS/Operations/VMNetworkOperationsBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.WindowsAzure.Commands.WAPackIaaS.Operations
+{
+    using DataContract;
+    using System;
+    using System.Collections.Generic;
+
+    public class VMNetworkOperationsBase : OperationsBase<VMNetwork>
+    {
+        public override VMNetwork Read(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<VMNetwork> Read()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
