14d43d97169b83531e47964978ce043117c6b293
Chris Tavares <ctavares@microsoft.com>
2013-10-09 17:39:33 -0700

    get-azuresubscription cmdlet

---
+++ WindowsAzurePowershell/src/Commands/Subscription/GetAzureAccount.cs
@@ -0,0 +1,60 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Commands.Subscription
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using Utilities.Common;
+    using Utilities.Subscription;
+
+    /// <summary>
+    /// Cmdlet to list the currently downloaded accounts and their
+    /// associated subscriptions.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "AzureAccount")]
+    public class GetAzureAccount : SubscriptionCmdletBase
+    {
+        public GetAzureAccount() : base(false)
+        {
+        }
+
+        [Parameter(Mandatory = false, HelpMessage = "Name of account to get information for")]
+        public string Name { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            IEnumerable<WindowsAzureSubscription> subscriptions = Profile.Subscriptions;
+            if (!string.IsNullOrEmpty(Name))
+            {
+                subscriptions = subscriptions.Where(s => s.ActiveDirectoryUserId == Name);
+            }
+
+            var sortedSubscriptions = from s in subscriptions
+                orderby s.ActiveDirectoryUserId ascending
+                select new
+                {
+                    Account = s.ActiveDirectoryUserId,
+                    s.SubscriptionName,
+                    Id = s.SubscriptionId,
+                    ActiveDirectoryTenantEndpoint = s.ActiveDirectoryEndpoint,
+                    s.ActiveDirectoryTenantId,
+                    s.IsDefault
+                };
+
+            WriteObject(sortedSubscriptions, true);
+        }
+    }
+}
