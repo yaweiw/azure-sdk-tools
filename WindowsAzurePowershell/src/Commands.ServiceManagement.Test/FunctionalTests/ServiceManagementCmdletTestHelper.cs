41d7588d35cd18fdaf7239fe3f216f765c73fb6a
unknown <Hyonho.Lee@microsoft.com>
2013-10-31 23:41:16 -0700

    Improve AclConfig test

--- WindowsAzurePowershell/src/Commands.ServiceManagement.Test/FunctionalTests/ServiceManagementCmdletTestHelper.cs
+++ WindowsAzurePowershell/src/Commands.ServiceManagement.Test/FunctionalTests/ServiceManagementCmdletTestHelper.cs
@@ -11,6 +11,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ----------------------------------------------------------------------------------
+
+using Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS.Endpoints;
+
 namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests
 {
     using Commands.Utilities.Common;
@@ -95,7 +98,15 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.ToString());
+                if (e is InvalidCastException)
+                {
+                    // continue
+                }
+                else
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
             }
 
             return resultCollection;
@@ -1305,6 +1316,11 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests
             RunPSCmdletAndReturnAll<NetworkAclObject>(new SetAzureAclConfigCmdletInfo(aclConfig.ToString(), aclObj, order,
                                                                               aclAction.ToString(), remoteSubnet, desc,
                                                                               null));
+        }
+
+        internal NetworkAclObject GetAzureAclConfig(PersistentVM vm, string ep = null)
+        {
+            return RunPSCmdletAndReturnFirst<NetworkAclObject>(new GetAzureAclConfigCmdletInfo(vm, ep));
         }
     }
 }
