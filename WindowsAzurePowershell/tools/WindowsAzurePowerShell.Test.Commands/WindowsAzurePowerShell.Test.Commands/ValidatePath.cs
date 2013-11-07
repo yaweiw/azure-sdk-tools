a882f485e745630c96e977b1338f6355e477c0a4
Abdelrahman Elogeel <abdelrahman.elogeel@microsoft.com>
2013-11-07 15:49:47 -0800

    Add verification

---
+++ WindowsAzurePowershell/tools/WindowsAzurePowerShell.Test.Commands/WindowsAzurePowerShell.Test.Commands/ValidatePath.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace WindowsAzurePowerShell.Test.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public sealed class ValidatePathAttribute : ValidateEnumeratedArgumentsAttribute
+    {
+        protected override void ValidateElement(object element)
+        {
+            string path = element.ToString();
+
+            if (!File.Exists(path))
+            {
+                throw new Exception("The provided path doesn't exist!");
+            }
+        }
+    }
+}
