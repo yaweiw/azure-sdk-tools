f11429768d9c81de174bc98624c738b96cd6ea79
markcowl <markcowl@microsoft.com>
2013-11-26 00:44:20 -0800

    Fix storage container test setup and cleanup

--- WindowsAzurePowershell/src/Commands.ScenarioTest/StorageTests/StorageContainerTest.cs
+++ WindowsAzurePowershell/src/Commands.ScenarioTest/StorageTests/StorageContainerTest.cs
@@ -25,6 +25,21 @@ namespace Microsoft.WindowsAzure.Commands.ScenarioTest.StorageTests
         {
         }
 
+        [TestInitialize]
+        public override void TestSetup()
+        {
+            base.TestSetup();
+            powershell.AddScript("Create-StorageAccount");
+        }
+
+        [TestCleanup]
+        public override void TestCleanup()
+        {
+            powershell.AddScript("Cleanup-StorageAccounts");
+            powershell.Invoke();
+            base.TestCleanup();
+        }
+
         [TestMethod]
         [TestCategory(Category.All)]
         [TestCategory(Category.Storage)]
