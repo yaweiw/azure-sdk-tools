a47785eef9d91cbd87cc614707d50de10d23e276
unknown <Hyonho.Lee@microsoft.com>
2014-01-15 14:49:43 -0800

    Updatee tests: ADDomainExtension, Static CA

--- WindowsAzurePowershell/src/Commands.ServiceManagement.Test/FunctionalTests/IaasCmdletInfo/NewAzureServiceCmdletInfo.cs
+++ WindowsAzurePowershell/src/Commands.ServiceManagement.Test/FunctionalTests/IaasCmdletInfo/NewAzureServiceCmdletInfo.cs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ----------------------------------------------------------------------------------
-using Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests.PowershellCore;
 
 namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests.IaasCmdletInfo
 {
@@ -19,21 +18,28 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests
 
     public class NewAzureServiceCmdletInfo : CmdletsInfo 
     {
-        public NewAzureServiceCmdletInfo(string serviceName, string serviceLabel, string servicelocation)
+        public NewAzureServiceCmdletInfo(string serviceName, string serviceLabel, string serviceLocation, string affinityGroupName)
         {
             this.cmdletName = Utilities.NewAzureServiceCmdletName;
 
             this.cmdletParams.Add(new CmdletParam("ServiceName", serviceName));
             this.cmdletParams.Add(new CmdletParam("Label", serviceLabel));
-            this.cmdletParams.Add(new CmdletParam("Location", servicelocation));
+            if (! string.IsNullOrEmpty(serviceLocation))
+            {
+                this.cmdletParams.Add(new CmdletParam("Location", serviceLocation));
+            }
+            if (! string.IsNullOrEmpty(affinityGroupName))
+            {
+                this.cmdletParams.Add(new CmdletParam("AffinityGroup", affinityGroupName));
+            }
         }
 
-        public NewAzureServiceCmdletInfo(string serviceName, string servicelocation)
+        public NewAzureServiceCmdletInfo(string serviceName, string serviceLocation)
         {
             this.cmdletName = Utilities.NewAzureServiceCmdletName;
 
             this.cmdletParams.Add(new CmdletParam("ServiceName", serviceName));            
-            this.cmdletParams.Add(new CmdletParam("Location", servicelocation));
+            this.cmdletParams.Add(new CmdletParam("Location", serviceLocation));
         }
     }
 }
---
+++ WindowsAzurePowershell/src/Commands.ServiceManagement.Test/FunctionalTests/PaasCmdletInfo/RemoveAzureServiceDomainJoinExtensionCmdletInfo.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests.PaasCmdletInfo
+{
+    using PowershellCore;
+
+    public class RemoveAzureServiceDomainJoinExtensionCmdletInfo:CmdletsInfo
+    {
+        public RemoveAzureServiceDomainJoinExtensionCmdletInfo(string serviceName, string slot, string[] role, bool uninstallConfiguration)
+        {
+            this.cmdletName = Utilities.RemoveAzureServiceDomainJoinExtension;
+            if(!string.IsNullOrEmpty(serviceName))
+            {
+                this.cmdletParams.Add(new CmdletParam("ServiceName", serviceName));
+            }
+            if(!string.IsNullOrEmpty(slot))
+            {
+                this.cmdletParams.Add(new CmdletParam("Slot", slot));
+            }
+            if(role != null)
+            {
+                this.cmdletParams.Add(new CmdletParam("Role", role));
+            }
+            if(uninstallConfiguration)
+            {
+                this.cmdletParams.Add(new CmdletParam("UninstallConfiguration"));
+            }
+        }
+    }
+}
