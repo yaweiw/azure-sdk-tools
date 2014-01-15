b7a473541add2736acfbe08d8f61eb0c5370de11
Pengfei Huang <phuang@microsoft.com>
2014-01-15 14:12:53 -0800

    Refactoring

--- WindowsAzurePowershell/src/Commands.ServiceManagement/IaaS/Extensions/RemoveAzureVMDiagnosticsExtension.cs
+++ WindowsAzurePowershell/src/Commands.ServiceManagement/IaaS/Extensions/RemoveAzureVMDiagnosticsExtension.cs
@@ -17,7 +17,6 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS.Extensions
     using System;
     using System.Management.Automation;
     using Model;
-    using Model.PersistentVMModel;
 
     [Cmdlet(VerbsCommon.Remove, "AzureVMDiagnosticsExtension"), OutputType(typeof(IPersistentVM))]
     public class RemoveAzureVMDiagnosticsExtensionCommand : VirtualMachineConfigurationCmdletBase
