0172c6bc513a27a4aa497c45600f7fa630b3d59f
v-didara <v-didara@microsoft.com>
2014-02-03 21:20:47 -0800

    Adding test cases for Get/Set/Remove-AzureVMBGInfoExtension cmdlets

---
+++ WindowsAzurePowershell/src/Commands.ServiceManagement.Test/FunctionalTests/IaasCmdletInfo/Extesnions/BGInfo/GetAzureVMBGInfoExtensionCmdletInfo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.WindowsAzure.Commands.ServiceManagement.Model;
+using Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests.PowershellCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Test.FunctionalTests.IaasCmdletInfo.Extensions.BGInfo
+{
+    public class GetAzureVMBGInfoExtensionCmdletInfo:CmdletsInfo
+    {
+        public GetAzureVMBGInfoExtensionCmdletInfo(IPersistentVM vm, string version = null, string referenceName = null)
+        {
+            cmdletName = Utilities.GetAzureVMBGInfoExtensionCmdletName;
+            cmdletParams.Add(new CmdletParam("VM", vm));
+            if (!string.IsNullOrEmpty(version))
+            {
+                cmdletParams.Add(new CmdletParam("Version", version));
+            }
+            if (!string.IsNullOrEmpty(referenceName))
+            {
+                cmdletParams.Add(new CmdletParam("ReferenceName", referenceName));
+            }
+        }
+    }
+
+}
