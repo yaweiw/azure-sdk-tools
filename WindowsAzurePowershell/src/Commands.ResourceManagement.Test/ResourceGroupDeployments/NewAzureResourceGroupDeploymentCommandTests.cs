3e805c66bf76958c194ce55829f5298565d134be
Abdelrahman Elogeel <abdelrahman.elogeel@microsoft.com>
2014-02-21 19:41:13 -0800

    Add missing files

---
+++ WindowsAzurePowershell/src/Commands.ResourceManagement.Test/ResourceGroupDeployments/NewAzureResourceGroupDeploymentCommandTests.cs
@@ -0,0 +1,201 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.ResourceManagement.Models;
+using Microsoft.Azure.Commands.ResourceManagement.ResourceGroupDeployments;
+using Microsoft.Azure.Management.Resources.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using Xunit;
+
+namespace Microsoft.Azure.Commands.ResourceManagement.Test.Resources
+{
+    public class NewAzureResourceGroupDeploymentCommandTests
+    {
+        private NewAzureResourceGroupDeploymentCommand cmdlet;
+
+        private Mock<ResourcesClient> resourcesClientMock;
+
+        private Mock<ICommandRuntime> commandRuntimeMock;
+
+        private string resourceGroupName = "myResourceGroup";
+
+        private string deploymentName = "fooDeployment";
+
+        private string templateFile = @"Resources\sampleTemplateFile.json";
+
+        private string parameterFile = @"Resources\sampleParameterFile.json";
+
+        private string storageAccountName = "myStorageAccount";
+
+        public NewAzureResourceGroupDeploymentCommandTests()
+        {
+            resourcesClientMock = new Mock<ResourcesClient>();
+            commandRuntimeMock = new Mock<ICommandRuntime>();
+            cmdlet = new NewAzureResourceGroupDeploymentCommand()
+            {
+                CommandRuntime = commandRuntimeMock.Object,
+                ResourceClient = resourcesClientMock.Object
+            };
+        }
+
+        [Fact]
+        public void CreatesNewPSResourceGroupDeploymentWithUserTemplate()
+        {
+            CreatePSResourceGroupDeploymentParameters expectedParameters = new CreatePSResourceGroupDeploymentParameters()
+            {
+                ParameterFile = parameterFile,
+                TemplateFile = templateFile,
+                DeploymentName = deploymentName,
+                StorageAccountName = storageAccountName,
+                TemplateHash = "hash",
+                TemplateHashAlgorithm = "Sha256",
+                TemplateVersion = "1.0"
+            };
+            CreatePSResourceGroupDeploymentParameters actualParameters = new CreatePSResourceGroupDeploymentParameters();
+            PSResourceGroupDeployment expected = new PSResourceGroupDeployment()
+            {
+                Mode = DeploymentMode.Incremental,
+                Name = deploymentName,
+                Outputs = new Dictionary<string, DeploymentVariable>()
+                {
+                    { "Variable1", new DeploymentVariable() { Value = "true", Type = "bool" } },
+                    { "Variable2", new DeploymentVariable() { Value = "10", Type = "int" } },
+                    { "Variable3", new DeploymentVariable() { Value = "hello world", Type = "string" } }
+                },
+                Parameters = new Dictionary<string, DeploymentVariable>()
+                {
+                    { "Parameter1", new DeploymentVariable() { Value = "true", Type = "bool" } },
+                    { "Parameter2", new DeploymentVariable() { Value = "10", Type = "int" } },
+                    { "Parameter3", new DeploymentVariable() { Value = "hello world", Type = "string" } }
+                },
+                ProvisioningState = ProvisioningState.Succeeded,
+                ResourceGroup = resourceGroupName,
+                TemplateLink = new TemplateLink()
+                {
+                    ContentHash = new ContentHash()
+                    {
+                        Algorithm = ContentHashAlgorithm.Sha256,
+                        Value = "hash"
+                    },
+                    ContentVersion = "1.0",
+                    Uri = new Uri("http://mytemplate.com")
+                },
+                Timestamp = new DateTime(2014, 2, 13)
+            };
+            resourcesClientMock.Setup(f => f.CreatePSResourceGroupDeployment(resourceGroupName,
+                It.IsAny<CreatePSResourceGroupDeploymentParameters>()))
+                .Returns(expected)
+                .Callback((string rg, CreatePSResourceGroupDeploymentParameters p) => { actualParameters = p; });
+
+            cmdlet.ResourceGroupName = resourceGroupName;
+            cmdlet.Name = expectedParameters.DeploymentName;
+            cmdlet.ParameterFile = expectedParameters.ParameterFile;
+            cmdlet.TemplateFile = expectedParameters.TemplateFile;
+            cmdlet.StorageAccountName = expectedParameters.StorageAccountName;
+            cmdlet.TemplateHash = expectedParameters.TemplateHash;
+            cmdlet.TemplateHashAlgorithm = expectedParameters.TemplateHashAlgorithm;
+            cmdlet.TemplateVersion = expectedParameters.TemplateVersion;
+
+            cmdlet.ExecuteCmdlet();
+
+            Assert.Equal(expectedParameters.DeploymentName, actualParameters.DeploymentName);
+            Assert.Equal(expectedParameters.GalleryTemplateName, actualParameters.GalleryTemplateName);
+            Assert.Equal(expectedParameters.TemplateFile, actualParameters.TemplateFile);
+            Assert.Equal(expectedParameters.ParameterObject, actualParameters.ParameterObject);
+            Assert.Equal(expectedParameters.ParameterFile, actualParameters.ParameterFile);
+            Assert.Equal(expectedParameters.TemplateVersion, actualParameters.TemplateVersion);
+            Assert.Equal(expectedParameters.TemplateHash, actualParameters.TemplateHash);
+            Assert.Equal(expectedParameters.TemplateHashAlgorithm, actualParameters.TemplateHashAlgorithm);
+            Assert.Equal(expectedParameters.StorageAccountName, actualParameters.StorageAccountName);
+
+            commandRuntimeMock.Verify(f => f.WriteObject(expected), Times.Once());
+        }
+
+        [Fact]
+        public void CreatesNewPSResourceGroupDeploymentWithGalleryTemplate()
+        {
+            CreatePSResourceGroupDeploymentParameters expectedParameters = new CreatePSResourceGroupDeploymentParameters()
+            {
+                ParameterFile = parameterFile,
+                GalleryTemplateName = "sqlServer",
+                DeploymentName = deploymentName,
+                StorageAccountName = storageAccountName,
+                TemplateHash = "hash",
+                TemplateHashAlgorithm = "Sha256",
+                TemplateVersion = "1.0"
+            };
+            CreatePSResourceGroupDeploymentParameters actualParameters = new CreatePSResourceGroupDeploymentParameters();
+            PSResourceGroupDeployment expected = new PSResourceGroupDeployment()
+            {
+                Mode = DeploymentMode.Incremental,
+                Name = deploymentName,
+                Outputs = new Dictionary<string, DeploymentVariable>()
+                {
+                    { "Variable1", new DeploymentVariable() { Value = "true", Type = "bool" } },
+                    { "Variable2", new DeploymentVariable() { Value = "10", Type = "int" } },
+                    { "Variable3", new DeploymentVariable() { Value = "hello world", Type = "string" } }
+                },
+                Parameters = new Dictionary<string, DeploymentVariable>()
+                {
+                    { "Parameter1", new DeploymentVariable() { Value = "true", Type = "bool" } },
+                    { "Parameter2", new DeploymentVariable() { Value = "10", Type = "int" } },
+                    { "Parameter3", new DeploymentVariable() { Value = "hello world", Type = "string" } }
+                },
+                ProvisioningState = ProvisioningState.Succeeded,
+                ResourceGroup = resourceGroupName,
+                TemplateLink = new TemplateLink()
+                {
+                    ContentHash = new ContentHash()
+                    {
+                        Algorithm = ContentHashAlgorithm.Sha256,
+                        Value = "hash"
+                    },
+                    ContentVersion = "1.0",
+                    Uri = new Uri("http://mytemplate.com")
+                },
+                Timestamp = new DateTime(2014, 2, 13)
+            };
+            resourcesClientMock.Setup(f => f.CreatePSResourceGroupDeployment(resourceGroupName,
+                It.IsAny<CreatePSResourceGroupDeploymentParameters>()))
+                .Returns(expected)
+                .Callback((string rg, CreatePSResourceGroupDeploymentParameters p) => { actualParameters = p; });
+
+            cmdlet.ResourceGroupName = resourceGroupName;
+            cmdlet.Name = expectedParameters.DeploymentName;
+            cmdlet.ParameterFile = expectedParameters.ParameterFile;
+            cmdlet.GalleryTemplateName = expectedParameters.GalleryTemplateName;
+            cmdlet.StorageAccountName = expectedParameters.StorageAccountName;
+            cmdlet.TemplateHash = expectedParameters.TemplateHash;
+            cmdlet.TemplateHashAlgorithm = expectedParameters.TemplateHashAlgorithm;
+            cmdlet.TemplateVersion = expectedParameters.TemplateVersion;
+
+            cmdlet.ExecuteCmdlet();
+
+            Assert.Equal(expectedParameters.DeploymentName, actualParameters.DeploymentName);
+            Assert.Equal(expectedParameters.GalleryTemplateName, actualParameters.GalleryTemplateName);
+            Assert.Equal(expectedParameters.TemplateFile, actualParameters.TemplateFile);
+            Assert.Equal(expectedParameters.ParameterObject, actualParameters.ParameterObject);
+            Assert.Equal(expectedParameters.ParameterFile, actualParameters.ParameterFile);
+            Assert.Equal(expectedParameters.TemplateVersion, actualParameters.TemplateVersion);
+            Assert.Equal(expectedParameters.TemplateHash, actualParameters.TemplateHash);
+            Assert.Equal(expectedParameters.TemplateHashAlgorithm, actualParameters.TemplateHashAlgorithm);
+            Assert.Equal(expectedParameters.StorageAccountName, actualParameters.StorageAccountName);
+
+            commandRuntimeMock.Verify(f => f.WriteObject(expected), Times.Once());
+        }
+    }
+}
---
+++ WindowsAzurePowershell/src/Commands.ResourceManagement/Models/ResourceManagementExtensions.cs
@@ -0,0 +1,146 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Management.Resources.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.ResourceManagement.Models
+{
+    public static class ResourceManagementExtensions
+    {
+        public static PSResourceGroup ToPSResourceGroup(this ResourceGroup resourceGroup, ResourcesClient client)
+        {
+            List<Resource> resources = client.FilterResources(new FilterResourcesOptions() { ResourceGroup = resourceGroup.Name });
+            return new PSResourceGroup()
+            {
+                Name = resourceGroup.Name,
+                Location = resourceGroup.Location,
+                Resources = resources,
+                ResourcesTable = ConstructResourcesTable(resources)
+            };
+        }
+
+        public static PSResourceGroupDeployment ToPSResourceGroupDeployment(this DeploymentOperationsCreateResult result, ResourcesClient client)
+        {
+            Dictionary<string, DeploymentVariable> outputs = new Dictionary<string, DeploymentVariable>();
+            Dictionary<string, DeploymentVariable> parameters = new Dictionary<string, DeploymentVariable>();
+            PSResourceGroupDeployment deploymentObject = new PSResourceGroupDeployment();
+
+            if (result != null)
+            {
+                deploymentObject.Name = result.Name;
+                deploymentObject.ResourceGroup = result.ResourceGroup;
+
+                if (result.Properties != null)
+                {
+                    deploymentObject.Mode = result.Properties.Mode;
+                    deploymentObject.ProvisioningState = result.Properties.ProvisioningState;
+                    deploymentObject.TemplateLink = result.Properties.TemplateLink;
+                    deploymentObject.Timestamp = result.Properties.Timestamp;
+
+                    if (!string.IsNullOrEmpty(result.Properties.Outputs))
+                    {
+                        outputs = JsonConvert.DeserializeObject<Dictionary<string, DeploymentVariable>>(result.Properties.Outputs);
+                        deploymentObject.Outputs = outputs;
+                        deploymentObject.OutputsString = ToString(outputs);
+                    }
+
+                    if (!string.IsNullOrEmpty(result.Properties.Parameters))
+                    {
+                        parameters = JsonConvert.DeserializeObject<Dictionary<string, DeploymentVariable>>(result.Properties.Parameters);
+                        deploymentObject.Parameters = parameters;
+                        deploymentObject.ParametersString = ToString(parameters);
+                    }
+
+                    if (result.Properties.TemplateLink != null)
+                    {
+                        deploymentObject.TemplateLinkString = ToString(result.Properties.TemplateLink);
+                    }
+                }
+            }
+
+            return deploymentObject;
+        }
+
+        private static string ConstructResourcesTable(List<Resource> resources)
+        {
+            StringBuilder resourcesTable = new StringBuilder();
+
+            if (resources.Count > 0)
+            {
+                string rowFormat = "{0, -15}  {1, -25}  {2, -10}\r\n";
+                resourcesTable.AppendLine();
+                resourcesTable.AppendFormat(rowFormat, "Name", "Type", "Location");
+                resourcesTable.AppendFormat(rowFormat, GenerateSeparator(15, "="), GenerateSeparator(25, "="), GenerateSeparator(10, "="));
+
+                foreach (Resource resource in resources)
+                {
+                    resourcesTable.AppendFormat(rowFormat, resource.Name, resource.Type, resource.Location);
+                }
+            }
+
+            return resourcesTable.ToString();
+        }
+
+        private static string GenerateSeparator(int amount, string separator)
+        {
+            string result = string.Empty;
+            while (amount-- != 0) result += separator;
+            return result;
+        }
+
+        private static string ToString(TemplateLink templateLink)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (templateLink != null)
+            {
+                result.AppendLine();
+                result.AppendLine(string.Format("{0, -15}: {1}", "Uri", templateLink.Uri));
+                result.AppendLine(string.Format("{0, -15}: {1}", "ContentVersion", templateLink.ContentVersion));
+                result.AppendLine(string.Format("{0, -15}:", "ContentHash"));
+
+                if (templateLink.ContentHash != null)
+                {
+                    result.AppendLine(string.Format("{0, -25}:", "Algorithm", templateLink.ContentHash.Algorithm));
+                    result.AppendLine(string.Format("{0, -25}:", "Value", templateLink.ContentHash.Value));
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static string ToString(Dictionary<string, DeploymentVariable> dictionary)
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (dictionary.Count > 0)
+            {
+                string rowFormat = "{0, -15}  {1, -25}  {2, -10}\r\n";
+                result.AppendLine();
+                result.AppendFormat(rowFormat, "Name", "Type", "Value");
+                result.AppendFormat(rowFormat, GenerateSeparator(15, "="), GenerateSeparator(25, "="), GenerateSeparator(10, "="));
+
+                foreach (KeyValuePair<string, DeploymentVariable> pair in dictionary)
+                {
+                    result.AppendFormat(rowFormat, pair.Key, pair.Value.Type, pair.Value.Value);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
