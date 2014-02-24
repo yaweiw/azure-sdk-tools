9daa3f95304a573db1982908e1c24863dbadf7f7
Shefali <shvohra@microsoft.com>
2014-02-24 15:28:17 -0800

    changes in response to code review

--- WindowsAzurePowershell/src/Commands.Storage.ScenarioTest/BVT/StorageBVT.cs
+++ WindowsAzurePowershell/src/Commands.Storage.ScenarioTest/BVT/StorageBVT.cs
@@ -14,7 +14,6 @@
 
 using Commands.Storage.ScenarioTest.Util;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Microsoft.WindowsAzure.Commands.ScenarioTest.Common;
 using Microsoft.WindowsAzure.Commands.ScenarioTest.Common;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
