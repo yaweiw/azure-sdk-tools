7ec7ae72010dea6fbcbd8081ac049fb41ce5c88c
huangpf <phuang@microsoft.com>
2014-02-28 14:33:55 -0800

    Add new fields to the Resource Extension class

--- WindowsAzurePowershell/src/Commands.ServiceManagement/Model/VMImageContext.cs
+++ WindowsAzurePowershell/src/Commands.ServiceManagement/Model/VMImageContext.cs
@@ -19,76 +19,17 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Model
 
     public class VMImageContext : ManagementOperationContext
     {
-        public string Name
-        {
-            get;
-            set;
-        }
-
-        public string Label
-        {
-            get;
-            set;
-        }
-
-        public string Category
-        {
-            get;
-            set;
-        }
-
-        public string Description
-        {
-            get;
-            set;
-        }
-
-        public Model.PersistentVMModel.OSDiskConfiguration OSDiskConfiguration
-        {
-            get;
-            set;
-        }
-
-        public Model.PersistentVMModel.DataDiskConfigurationList DataDiskConfigurations
-        {
-            get;
-            set;
-        }
-
-        public string ServiceName
-        {
-            get;
-            set;
-        }
-
-        public string DeploymentName
-        {
-            get;
-            set;
-        }
-
-        public string RoleName
-        {
-            get;
-            set;
-        }
-
-        public string Location
-        {
-            get;
-            set;
-        }
-
-        public string AffinityGroup
-        {
-            get;
-            set;
-        }
-
-        public DateTime? CreatedTime
-        {
-            get;
-            set;
-        }
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public Model.PersistentVMModel.OSDiskConfiguration OSDiskConfiguration { get; set; }
+        public Model.PersistentVMModel.DataDiskConfigurationList DataDiskConfigurations { get; set; }
+        public string ServiceName { get; set; }
+        public string DeploymentName { get; set; }
+        public string RoleName { get; set; }
+        public string Location { get; set; }
+        public string AffinityGroup { get; set; }
+        public DateTime? CreatedTime { get; set; }
     }
 }
