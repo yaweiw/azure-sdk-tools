2e026304103d292e6e7fb37948934ebb99a6efed
Abdelrahman Elogeel <abdelrahman.elogeel@microsoft.com>
2014-03-12 10:28:29 -0700

    Fix warnings & usings

--- WindowsAzurePowershell/src/Common/WindowsAzureSubscription.cs
+++ WindowsAzurePowershell/src/Common/WindowsAzureSubscription.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.WindowsAzure.Commands.Common;
-// ----------------------------------------------------------------------------------
+﻿// ----------------------------------------------------------------------------------
 //
 // Copyright Microsoft Corporation
 // Licensed under the Apache License, Version 2.0 (the "License");
