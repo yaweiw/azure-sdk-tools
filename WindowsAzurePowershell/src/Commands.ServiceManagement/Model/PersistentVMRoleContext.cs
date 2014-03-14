b47774dbaa46af9cb2d0a91cc761d22c1cc12efe
huangpf <phuang@microsoft.com>
2014-03-13 23:01:19 -0700

    Update Get-AzureVM, Get-AzureVMExtension

--- WindowsAzurePowershell/src/Commands.ServiceManagement/Model/PersistentVMRoleContext.cs
+++ WindowsAzurePowershell/src/Commands.ServiceManagement/Model/PersistentVMRoleContext.cs
@@ -18,34 +18,21 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Model
     public class PersistentVMRoleContext : ServiceOperationContext, IPersistentVM
     {
         public string DeploymentName { get; set; }
-
         public string Name { get; set; }
-
         public string Label { get; set; }
-
         public PersistentVM VM { get; set; }
-
         public string InstanceStatus { get; set; }
-
         public string IpAddress { get; set; }
-
         public string InstanceStateDetails { get; set; }
-
         public string PowerState { get; set; }
-
         public string InstanceErrorCode { get; set; }
-
         public string InstanceFaultDomain { get; set; }
-
         public string InstanceName { get; set; }
-
         public string InstanceUpgradeDomain { get; set; }
-
         public string InstanceSize { get; set; }
-
         public string AvailabilitySetName { get; set; }
-
         public string DNSName { get; set; }
+        public string Status { get; set; }
 
         public PersistentVM GetInstance()
         {
