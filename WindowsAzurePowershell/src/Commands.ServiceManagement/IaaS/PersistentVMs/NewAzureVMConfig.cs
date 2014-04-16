0426599623c6d4d6e5906feb1c3dbd900b4696e1
huangpf <phuang@microsoft.com>
2014-04-15 20:36:35 -0700

    address cr

--- WindowsAzurePowershell/src/Commands.ServiceManagement/IaaS/PersistentVMs/NewAzureVMConfig.cs
+++ WindowsAzurePowershell/src/Commands.ServiceManagement/IaaS/PersistentVMs/NewAzureVMConfig.cs
@@ -12,142 +12,148 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
+
 namespace Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS
 {
     using System;
     using System.Collections.ObjectModel;
     using System.Management.Automation;
+    using Commands.Utilities.Common;
     using Model;
     using Model.PersistentVMModel;
     using Properties;
-    using Utilities.Common;
 
-    [Cmdlet(
-        VerbsCommon.New,
-        AzureVMConfigNoun,
-        DefaultParameterSetName = ImageNameParamSet),
-    OutputType(
-        typeof(PersistentVM))]
+    [Cmdlet(VerbsCommon.New, "AzureVMConfig", DefaultParameterSetName = "ImageName"), OutputType(typeof(PersistentVM))]
     public class NewAzureVMConfigCommand : PSCmdlet
     {
-        private const string AzureVMConfigNoun = "AzureVMConfig";
-        private const string ImageNameParamSet = "ImageName";
-        private const string DiskNameParamSet = "DiskName";
-
         private const string RoleType = "PersistentVMRole";
 
-        [Parameter(
-            Position = 0,
-            Mandatory = true,
-            ValueFromPipelineByPropertyName = true,
-            HelpMessage = "The virtual machine name.")]
+        [Parameter(Position = 0, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The virtual machine name.")]
         [ValidateNotNullOrEmpty]
-        public string Name { get; set; }
-
-        [Parameter(
-            Position = 1,
-            Mandatory = true,
-            ValueFromPipelineByPropertyName = true,
-            HelpMessage = "Represents the size of the machine.")]
-        [ValidateNotNullOrEmpty]
-        public string InstanceSize { get; set; }
-
-        [Parameter(
-            Position = 2,
-            HelpMessage = "Controls the platform caching behavior of the OS disk.")]
-        [ValidateSet(
-            "ReadWrite",
-            "ReadOnly",
-            IgnoreCase = true)]
-        public string HostCaching { get; set; }
-
-        [Parameter(
-            Position = 3,
-            HelpMessage = "The name of the availability set.")]
-        [ValidateNotNullOrEmpty]
-        public string AvailabilitySetName { get; set; }
-
-        [Parameter(
-            Position = 4,
-            ValueFromPipelineByPropertyName = true,
-            HelpMessage = "The VM label.")]
-        [ValidateNotNullOrEmpty]
-        public string Label { get; set; }
-
-        [Parameter(
-            ParameterSetName = DiskNameParamSet,
-            Position = 5,
-            Mandatory = true,
-            HelpMessage = "Friendly name of the OS disk in the disk repository.")]
-        [ValidateNotNullOrEmpty]
-        public string DiskName { get; set; }
-
-        [Parameter(
-            ParameterSetName = ImageNameParamSet,
-            Position = 5,
-            Mandatory = true,
-            HelpMessage = "Reference to a platform stock image or a user image " +
-                          "from the image repository.")]
-        [ValidateNotNullOrEmpty]
-        public string ImageName { get; set; }
-
-        [Parameter(
-            ParameterSetName = ImageNameParamSet,
-            Position = 6,
-            HelpMessage = "Location of the where the VHD should be created. " +
-                          "This link refers to a blob in a storage account. " +
-                          "If not specified the VHD will be created in the " +
-                          "default storage account with the following format: " +
-                          "'vhds/servicename-vmname-year-month-day-ms'")]
-        [ValidateNotNullOrEmpty]
-        public string MediaLocation { get; set; }
-
-        [Parameter(
-            ParameterSetName = ImageNameParamSet,
-            Position = 7,
-            HelpMessage = "Label of the new disk to be created.")]
-        [ValidateNotNullOrEmpty]
-        public string DiskLabel { get; set; }
-
-        protected override void ProcessRecord()
+        public string Name
         {
-            base.ProcessRecord();
+            get;
+            set;
+        }
 
+        [Parameter(Position = 1, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "Represents the size of the machine.")]
+        [ValidateNotNullOrEmpty]
+        public string InstanceSize
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 2, HelpMessage = "Controls the platform caching behavior of the OS disk.")]
+        [ValidateSet("ReadWrite", "ReadOnly", IgnoreCase = true)]
+        public string HostCaching
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 3, HelpMessage = "The name of the availability set.")]
+        [ValidateNotNullOrEmpty]
+        public string AvailabilitySetName
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 4, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "The VM label.")]
+        [ValidateNotNullOrEmpty]
+        public string Label
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 5, Mandatory = true, ParameterSetName = "DiskName", HelpMessage = "Friendly name of the OS disk in the disk repository.")]
+        [ValidateNotNullOrEmpty]
+        public string DiskName
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 5, Mandatory = true, ParameterSetName = "ImageName", HelpMessage = "Reference to a platform stock image or a user image from the image repository.")]
+        [ValidateNotNullOrEmpty]
+        public string ImageName
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 6, Mandatory = false, ParameterSetName = "ImageName", HelpMessage = "Location of the where the VHD should be created. This link refers to a blob in a storage account. If not specified the VHD will be created in the default storage account with the following format :vhds/servicename-vmname-year-month-day-ms")]
+        [ValidateNotNullOrEmpty]
+        public string MediaLocation
+        {
+            get;
+            set;
+        }
+
+        [Parameter(Position = 7, Mandatory = false, ParameterSetName = "ImageName", HelpMessage = "Label of the new disk to be created.")]
+        [ValidateNotNullOrEmpty]
+        public string DiskLabel
+        {
+            get;
+            set;
+        }
+
+        internal void ExecuteCommand()
+        {
             ValidateParameters();
 
-            var role = new PersistentVM
+            if (string.IsNullOrEmpty(Label))
             {
-                AvailabilitySetName  = AvailabilitySetName,
-                RoleName             = Name,
-                RoleSize             = InstanceSize,
-                RoleType             = RoleType,
-                Label                = string.IsNullOrEmpty(Label) ? Name : Label,
-                ProvisionGuestAgent  = true,
-                ConfigurationSets    = new Collection<ConfigurationSet>(),
+                Label = Name;
+            }
+
+            Label = Label;
+
+            var role = new PersistentVM 
+            {
+                AvailabilitySetName = AvailabilitySetName,
+                ConfigurationSets = new Collection<ConfigurationSet>(),
                 DataVirtualHardDisks = new Collection<DataVirtualHardDisk>(),
-                OSVirtualHardDisk    = new OSVirtualHardDisk
-                {
-                    DiskName        = DiskName,
-                    SourceImageName = ImageName,
-                    MediaLink       = string.IsNullOrEmpty(MediaLocation) ? null : new Uri(MediaLocation),
-                    HostCaching     = HostCaching,
-                    DiskLabel       = string.IsNullOrEmpty(DiskLabel) ? null : DiskLabel
-                }
+                RoleName = Name,
+                RoleSize = InstanceSize,
+                RoleType = RoleType,
+                Label = Label,
+                ProvisionGuestAgent = true
+            };
+
+            role.OSVirtualHardDisk = new OSVirtualHardDisk()
+            {
+                DiskName = DiskName,
+                SourceImageName = ImageName,
+                MediaLink = string.IsNullOrEmpty(MediaLocation) ? null : new Uri(MediaLocation),
+                HostCaching = HostCaching,
+                DiskLabel = string.IsNullOrEmpty(DiskLabel) ? null : DiskLabel
             };
 
             WriteObject(role, true);
         }
 
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                base.ProcessRecord();
+                ExecuteCommand();
+            }
+            catch (Exception ex)
+            {
+                WriteError(new ErrorRecord(ex, string.Empty, ErrorCategory.CloseError, null));
+            }
+        }
+
         protected void ValidateParameters()
         {
-            var currentSubscription = WindowsAzureProfile.Instance.CurrentSubscription;
-            bool noCurrentStorageAccountFound = currentSubscription == null
-                   || currentSubscription.CurrentStorageAccountName == null;
-
-            if (noCurrentStorageAccountFound && MediaLocation == null)
+            WindowsAzureSubscription currentSubscription = WindowsAzureProfile.Instance.CurrentSubscription;
+            if ((currentSubscription == null || currentSubscription.CurrentStorageAccountName == null) && MediaLocation == null)
             {
-                throw new ArgumentException(
-                    Resources.MustSpecifyMediaLocationOrHaveCurrentStorageAccount);
+                throw new ArgumentException(Resources.MustSpecifyMediaLocationOrHaveCurrentStorageAccount);
             }
         }
     }
