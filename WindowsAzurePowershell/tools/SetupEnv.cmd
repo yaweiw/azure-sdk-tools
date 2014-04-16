7102b5e008c1752325148caa422df6d765397c33
yugangw-msft <yugangw@microsoft.com>
2014-04-16 15:32:26 -0700

    update setup command to use team server for accessing common command

--- WindowsAzurePowershell/tools/SetupEnv.cmd
+++ WindowsAzurePowershell/tools/SetupEnv.cmd
@@ -4,12 +4,24 @@ if defined AzurePSRoot exit /b 0
 
 echo Initializing environment...
 
-if exist "%USERPROFILE%\SetNugetFeed.cmd" call "%USERPROFILE%\SetNugetFeed.cmd"
+if defined PRIVATE_SETTING_CMD (
+    call "%PRIVATE_SETTING_CMD%"
+) else (
+    call "\\vwdbuild01\dev\AdxSdk\SetPowerShellEnvVars.cmd"
+)
 
 if not defined PRIVATE_FEED_URL (
     echo Error, please set following environment variables so that build script can download Spec Nuget Packages:
     echo     PRIVATE_FEED_URL, PRIVATE_FEED_USER_NAME and PRIVATE_FEED_PASSWORD
 	exit /b 1
+)
+
+::PowerShell commands need elevation for dependencies installation and running tests
+::Here we invoke an elevation-needing command to test it
+net session > NUL 2>&1
+if ERRORLEVEL 1 (
+    ECHO ERROR: Please launch command under administrator account. It is needed for environment setting up and unit test.
+    EXIT /B 1
 )
 
 set "AzurePSRoot=%~dp0"
@@ -19,10 +31,13 @@ set "AzurePSRoot=%AzurePSRoot:~0,-7%"
 if defined ProgramFiles(x86) (
     set "ADXSDKProgramFiles=%ProgramFiles(x86)%"
     set "ADXSDKPlatform=x64"
+    set "wow64RegKey=\Wow6432Node"
 ) else (
     set "ADXSDKProgramFiles=%ProgramFiles%"
     set "ADXSDKPlatform=x86"
 )
+:: Update policy, so azure powershell can run
+reg add "HKLM\SOFTWARE%wow64RegKey%\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /f /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted"
 
 if exist "%ADXSDKProgramFiles%\Microsoft Visual Studio 12.0" (
     set ADXSDKVSVersion=12.0
