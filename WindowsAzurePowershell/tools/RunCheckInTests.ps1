ddd2b3387938739a150f94607a515ad97db0085d
yugangw-msft <yugangw@microsoft.com>
2014-04-22 15:01:17 -0700

    add copy right header

--- WindowsAzurePowershell/tools/RunCheckInTests.ps1
+++ WindowsAzurePowershell/tools/RunCheckInTests.ps1
@@ -1,3 +1,16 @@
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
 
 $scriptFolder = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . ($scriptFolder + '.\SetupEnv.ps1')
@@ -40,7 +53,7 @@ try {
 }
 catch [System.Management.Automation.CommandNotFoundException] {
     if (Test-Path "$env:ADXSDKProgramFiles\Git\bin") {
-        Write-Host Adding Git installation folder to the PATH environment variable, needed for 2 unit tests
+        Write-Host Adding Git installation folder to the PATH environment variable, needed for some unit tests.
         $env:path = $env:path + ";$env:ADXSDKProgramFiles\Git\bin"
     }
 }
