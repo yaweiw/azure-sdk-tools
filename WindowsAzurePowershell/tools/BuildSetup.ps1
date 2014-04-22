49670cb7879148e3cc80a6228a94d21765955e45
yugangw-msft <yugangw@microsoft.com>
2014-04-22 13:40:52 -0700

    Ensure build before build setup

--- WindowsAzurePowershell/tools/BuildSetup.ps1
+++ WindowsAzurePowershell/tools/BuildSetup.ps1
@@ -1,5 +1,5 @@
 $scriptFolder = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . ($scriptFolder + '.\SetupEnv.ps1')
 
-msbuild "$env:AzurePSRoot\..\build.proj" /t:buildsetupdebug
+msbuild "$env:AzurePSRoot\..\build.proj" /t:"BuildDebug;buildsetupdebug"
 Write-Host MSI file path: %AzurePSRoot%\setup\build\Debug\x86
