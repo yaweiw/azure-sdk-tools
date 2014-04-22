da5c711926c4ba83352ec49b4a824a78157a8f83
yugangw-msft <yugangw@microsoft.com>
2014-04-22 13:42:47 -0700

    fix up the msi file path

--- WindowsAzurePowershell/tools/BuildSetup.ps1
+++ WindowsAzurePowershell/tools/BuildSetup.ps1
@@ -2,4 +2,4 @@ $scriptFolder = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . ($scriptFolder + '.\SetupEnv.ps1')
 
 msbuild "$env:AzurePSRoot\..\build.proj" /t:"BuildDebug;buildsetupdebug"
-Write-Host MSI file path: %AzurePSRoot%\setup\build\Debug\x86
+Write-Host "MSI file path: $env:AzurePSRoot\setup\build\Debug\x86\windowsazure-powershell.msi"
