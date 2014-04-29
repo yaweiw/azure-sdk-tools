7b3c5a3f468742a781adbafc95e3105dc4f5b301
huangpf <phuang@microsoft.com>
2014-04-28 19:24:20 -0700

    Add -Force to Remove-AzureReservedIP cmdlet

--- WindowsAzurePowershell/src/Commands.ServiceManagement/Properties/Resources.Designer.cs
+++ WindowsAzurePowershell/src/Commands.ServiceManagement/Properties/Resources.Designer.cs
@@ -1141,6 +1141,24 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Are you sure you want to remove the Reserved IP from your subscription?.
+        /// </summary>
+        internal static string RemoveReservedIPWarning {
+            get {
+                return ResourceManager.GetString("RemoveReservedIPWarning", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Remove the Reserved IP from your subscription..
+        /// </summary>
+        internal static string RemoveReservedIPWhatIfMessage {
+            get {
+                return ResourceManager.GetString("RemoveReservedIPWhatIfMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Reserved IP with the Name:&apos;{0}&apos; will no longer be in use after the deployment is deleted or stopped (deallocated), and it is still reserved for later use. Please note that the Reserved IPs that are not associated with a deployment might be billed..
         /// </summary>
         internal static string ReservedIPNameNoLongerInUseButStillBeingReserved {
