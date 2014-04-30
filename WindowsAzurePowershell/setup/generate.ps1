efc91da2aeb14379a339d411fbfad7b12e48a837
Abdelrahman Elogeel <abdelrahman.elogeel@microsoft.com>
2014-04-29 17:22:22 -0700

    Installer bug fixes: - Installer now sets the PSModulePath to <install dir>\ServiceManagement - The shortcut execution policy is RemoteSigned - Changed BuildInstaller.ps1 to remove pdbs, *.lastsuccessfullbuild and *.dll.config files

--- WindowsAzurePowershell/setup/generate.ps1
+++ WindowsAzurePowershell/setup/generate.ps1
@@ -3,33 +3,33 @@ param(
     [string] $buildConfig
 )
 
-if ($buildConfig -eq $null -or ($args[0] -ne "Debug" -and $args -ne "Release"))
+if ([string]::IsNullOrEmpty($buildConfig))
 {
+	Write-Verbose "Setting build configuration to 'Release'"
 	$buildConfig = 'Release'
 }
 
 Write-Verbose "Build configuration is set to $buildConfig"
+
 $output = Join-Path $(Split-Path -Parent -Path (Split-Path -Parent -Path $PSScriptRoot)) $(Join-Path 'Package' $buildConfig)
 $serviceManagementPath = Join-Path $output "ServiceManagement\Azure"
 $resourceManagerPath = Join-Path $output "ResourceManager\AzureResourceManager"
 $VerbosePreference = 'Continue'
 
 Write-Verbose "Removing duplicated AzureProfile.psd1..."
-Remove-Item -Force $serviceManagementPath\AzureProfile.psd1
+Remove-Item -Force $serviceManagementPath\AzureProfile.psd1 -ErrorAction SilentlyContinue
 
 Write-Verbose "Removing Resources folder $serviceManagementPath\Resources\..."
-Remove-Item -Recurse -Force $serviceManagementPath\Resources\
+Remove-Item -Recurse -Force $serviceManagementPath\Resources\ -ErrorAction SilentlyContinue
 
-Write-Verbose "Removing generated NuGet files from $output"
+Write-Verbose "Removing generated NuGet folders from $output"
 $resourcesFolders = @("de", "es", "fr", "it", "ja", "ko", "ru", "zh-Hans", "zh-Hant")
-Get-ChildItem -Include $resourcesFolders -Recurse -Force -Path $output | Remove-Item -Force -Recurse
+Get-ChildItem -Include $resourcesFolders -Recurse -Force -Path $output | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
 
 Write-Verbose "Removing XML help files for helper dlls from $output"
-$execlusions = @("*.dll-Help.xml", "Scaffold.xml", "RoleSettings.xml", "WebRole.xml", "WorkerRole.xml")
-Get-ChildItem -Include *.xml -Exclude $execlusions -Recurse -Path $output | Remove-Item -Force -Recurse
-
-Write-Verbose "Removing config files for dlls from $output"
-Remove-Item -Recurse -Force $output\*.dll.config
+$exclude = @("*.dll-Help.xml", "Scaffold.xml", "RoleSettings.xml", "WebRole.xml", "WorkerRole.xml")
+$include = @("*.xml", "*.lastcodeanalysissucceeded", "*.dll.config", "*.pdb")
+Get-ChildItem -Include $include -Exclude $exclude -Recurse -Path $output | Remove-Item -Force -Recurse
 
 if (Get-Command "heat.exe" -ErrorAction SilentlyContinue)
 {
@@ -37,8 +37,8 @@ if (Get-Command "heat.exe" -ErrorAction SilentlyContinue)
     heat dir $output -srd -gg -g1 -cg azurecmdfiles -sfrag -dr PowerShellFolder -var var.sourceDir -o $(Join-Path $PSScriptRoot 'azurecmdfiles.wxi')
     
 	# Replace <Wix> with <Include>
-	(gc $azureFiles).replace('<Wix','<Include') | Set-Content $azureFiles
-	(gc $azureFiles).replace('</Wix','</Include') | Set-Content $azureFiles
+	(gc $azureFiles).replace('<Wix', '<Include') | Set-Content $azureFiles
+	(gc $azureFiles).replace('</Wix' ,'</Include') | Set-Content $azureFiles
 }
 else
 {
