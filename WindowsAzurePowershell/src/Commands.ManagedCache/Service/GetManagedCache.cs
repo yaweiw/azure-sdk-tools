8d5bf25f4eb621b0b6a16ce7eb892c12f06eed86
yugangw-msft <yugangw@microsoft.com>
2014-04-30 09:34:23 -0700

    Initial check in for managed cache powershell project

---
+++ WindowsAzurePowershell/src/Commands.ManagedCache/Service/GetManagedCache.cs
@@ -0,0 +1,69 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Commands.ManagedCache
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Management.Automation;
+    using Microsoft.WindowsAzure.Commands.Utilities.Common;
+    using Microsoft.WindowsAzure.Management.ManagedCache;
+
+    /// <summary>
+    /// Retrieves a list of Windows Azure SQL Database servers in the selected subscription.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "AzureManagedCache", ConfirmImpact = ConfirmImpact.None)]
+    public class GetManagedCache : ManagedCacheCmdletBase
+    {
+        [Parameter(Position = 0, ValueFromPipelineByPropertyName = true,
+            HelpMessage = "azure cache service name.")]
+        [ValidateNotNullOrEmpty]
+        public string CacheServiceName
+        {
+            get;
+            set;
+        }
+
+        internal IntrinsicSettings.CacheServiceInput GetCacheService(string cacheService)
+        {
+            CloudServiceListResponse listResp = CacheClient.CloudServices.List();
+            IntrinsicSettings.CacheServiceInput matchedCacheService = null;
+            foreach(var cloudService in listResp)
+            {
+                //TODO: use const for caching type and provider namespace
+                CloudServiceListResponse.CloudService.AddOnResource matched = cloudService.Resources.FirstOrDefault(
+                    p => { return p.Type == "Caching" && cacheService.Equals(p.Name, StringComparison.OrdinalIgnoreCase); }
+                    );
+
+                if (matched != null)
+                {
+                    matchedCacheService = matched.IntrinsicSettingsSection.CacheServiceInputSection;
+                }
+            }
+            return matchedCacheService;
+        }
+
+        public override void ExecuteCmdlet()
+        {
+            var cacheService = this.GetCacheService(CacheServiceName);
+            if (cacheService == null)
+            {
+                throw new ArgumentException("Invalid name"); //TODO: using resource string
+            }
+            WriteObject(cacheService);
+        }      
+    }
+}
