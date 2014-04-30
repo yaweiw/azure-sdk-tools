b4f8efe6a85b4821579f4f1cfa3cb8a0d209f441
yugangw-msft <yugangw@microsoft.com>
2014-04-30 09:34:23 -0700

    Initial check in for managed cache powershell project

---
+++ WindowsAzurePowershell/src/Commands.ManagedCache/ManagedCacheCmdletBase.cs
@@ -0,0 +1,44 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.WindowsAzure.Commands.ManagedCache
+{
+    using System;
+    using System.Globalization;
+    using System.Management.Automation;
+
+    using Microsoft.WindowsAzure.Commands.Utilities;
+    using Microsoft.WindowsAzure.Commands.Utilities.Common;
+    using Microsoft.WindowsAzure.Management.ManagedCache;
+
+    /// <summary>
+    /// The base class for all Windows Azure Managed Cache Management Cmdlets
+    /// </summary>
+    public abstract class ManagedCacheCmdletBase : CmdletWithSubscriptionBase
+    {
+        private ManagedCacheClient cacheClient;
+
+        public ManagedCacheClient CacheClient
+        {
+            get
+            {
+                if (cacheClient == null)
+                {
+                    cacheClient = CurrentSubscription.CreateClient <ManagedCacheClient>();
+                }
+                return cacheClient;
+            }
+        }
+    }
+}
