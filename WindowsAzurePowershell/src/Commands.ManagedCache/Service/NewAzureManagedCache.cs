fe534bc818fd9d58b3b4d63f7c55a1b6d6cd45fb
yugangw-msft <yugangw@microsoft.com>
2014-05-01 10:12:29 -0700

    Create convenient client code

--- WindowsAzurePowershell/src/Commands.ManagedCache/Service/NewAzureManagedCache.cs
+++ WindowsAzurePowershell/src/Commands.ManagedCache/Service/NewAzureManagedCache.cs
@@ -30,7 +30,6 @@ namespace Microsoft.Azure.Commands.ManagedCache
     [Cmdlet(VerbsCommon.New, "AzureManagedCache", ConfirmImpact = ConfirmImpact.None)]
     public class NewAzureManagedCache : ManagedCacheCmdletBase
     {
-        private string cloudServiceName;
         private string cacheServiceName;
 
         [Parameter(Position = 0, 
@@ -74,145 +73,16 @@ namespace Microsoft.Azure.Commands.ManagedCache
         {
             //TODO, validate the Name, length much be between 6~20;
             //Only lower case letter and number, and start with letter
-            EnsureCloudService();
-
             cacheServiceName = Name.ToLower();
-            CloudServiceGetResponse.Resource newCacheService = CreateCacheServices();
-            WriteObject(newCacheService);
-        }
 
-        private void EnsureCloudService()
-        {
-            cloudServiceName = GetCloudServiceName(CurrentSubscription.SubscriptionId, Location);
-            if (!CloudServiceExists())
-            {
-                CloudServiceCreateParameters parameters = new CloudServiceCreateParameters();
-                parameters.GeoRegion = Location;
-                parameters.Description = cloudServiceName;
-                parameters.Label = cloudServiceName;
-                OperationResponse response = CacheClient.CloudServices.Create(cloudServiceName, parameters);
-            }
-        }
-
-        public CloudServiceGetResponse.Resource CreateCacheServices()
-        {
-            CacheServiceCreateParameters param = new CacheServiceCreateParameters();
-            IntrinsicSettings settings = new IntrinsicSettings();
-            IntrinsicSettings.CacheServiceInput input = new IntrinsicSettings.CacheServiceInput();
-            settings.CacheServiceInputSection = input;
-            param.Settings = settings;
-            input.Location = Location;
-            input.SkuCount = 1; //TODO
-            input.ServiceVersion = "1.0.0";
-            input.ObjectSizeInBytes = 1024;
-            input.SkuType = Sku;
-
-            CacheClient.CacheServices.CreateCacheService(cloudServiceName, cacheServiceName, param);
-
-            CloudServiceGetResponse.Resource newCacheResource = null;
-            //service state goes through Creating/Updating to Active. WE only care about active
-            int waitInMinutes = 30; //minutes
-            while (waitInMinutes > 0)
-            {
-                newCacheResource = GetCacheService();
-                if (newCacheResource.SubState == CACHE_SERVICE_READY_STATE)
-                {
-                    break;
-                }
-                else
-                {
-                    //TODO: use PS fancy progress ?
-                    System.Threading.Thread.Sleep(60000);
-                    waitInMinutes--;
-                }
-            }
-
-            if (waitInMinutes < 0)
-            {
-                throw new InvalidOperationException("Time out to wait for cache service ready");
-            }
-            return newCacheResource;
-        }
-
-        public CloudServiceGetResponse.Resource GetCacheService()
-        {
-            CloudServiceGetResponse resp = CacheClient.CloudServices.Get(cloudServiceName);
-            CloudServiceGetResponse.Resource cacheResource = null;
-            foreach (var resource in resp.Resources)
-            {
-                if (resource.Type == CACHE_RESOURCE_TYPE && 
-                    resource.Name.Equals(cacheServiceName, StringComparison.OrdinalIgnoreCase))
-                {
-                    cacheResource = resource;
-                }
-            }
+            WriteVerbose(string.Format(Properties.Resources.CreatingCacheServiceStarted, cacheServiceName));
             
-            if (cacheResource == null)
-            {
-                //TODO: use resource strings
-                throw new InvalidOperationException("Not able to find the cache service just now created");
-            }
-            return cacheResource;
+            WriteObject(CacheClient.CreateCacheService(
+                CurrentSubscription.SubscriptionId,
+                cacheServiceName,
+                Location,
+                Sku,
+                Memory));
         }
-
-        public bool CloudServiceExists()
-        {
-            try
-            {
-                CloudServiceGetResponse response = CacheClient.CloudServices.Get(cloudServiceName);
-            }
-            catch(CloudException ex)
-            {
-                if (ex.Response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                {
-                    return false;
-                }
-                throw ex;
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// The following logic was ported from Azure Cache management portal. It is critical to maintain the 
-        /// parity. Do not modify unless you understand the consequence.
-        /// </summary>
-        public string GetCloudServiceName(string subscriptionId, string region)
-        {
-            string hashedSubId = string.Empty;
-            string extensionPrefix = CACHE_RESOURCE_TYPE;
-            using (SHA256 sha256 = SHA256Managed.Create())
-            {
-                hashedSubId = Base32NoPaddingEncode(sha256.ComputeHash(UTF8Encoding.UTF8.GetBytes(subscriptionId)));
-            }
-
-            return string.Format(CultureInfo.InvariantCulture, "{0}{1}-{2}", extensionPrefix, hashedSubId, region.Replace(' ', '-'));
-        }
-
-        private string Base32NoPaddingEncode(byte[] data)
-        {
-            const string Base32StandardAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
-
-            StringBuilder result = new StringBuilder(Math.Max((int)Math.Ceiling(data.Length * 8 / 5.0), 1));
-
-            byte[] emptyBuffer = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 };
-            byte[] workingBuffer = new byte[8];
-
-            // Process input 5 bytes at a time
-            for (int i = 0; i < data.Length; i += 5)
-            {
-                int bytes = Math.Min(data.Length - i, 5);
-                Array.Copy(emptyBuffer, workingBuffer, emptyBuffer.Length);
-                Array.Copy(data, i, workingBuffer, workingBuffer.Length - (bytes + 1), bytes);
-                Array.Reverse(workingBuffer);
-                ulong val = BitConverter.ToUInt64(workingBuffer, 0);
-
-                for (int bitOffset = ((bytes + 1) * 8) - 5; bitOffset > 3; bitOffset -= 5)
-                {
-                    result.Append(Base32StandardAlphabet[(int)((val >> bitOffset) & 0x1f)]);
-                }
-            }
-
-            return result.ToString();
-        } 
     }
 }
