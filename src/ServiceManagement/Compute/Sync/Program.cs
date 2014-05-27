1e37e861145c554ab4617d092478344a54a9374a
huangpf <phuang@microsoft.com>
2014-05-27 15:19:09 -0700

    Remove ConsoleAppUtils.csproj

--- src/ServiceManagement/Compute/Sync/Program.cs
+++ src/ServiceManagement/Compute/Sync/Program.cs
@@ -14,7 +14,6 @@
 
 namespace Microsoft.WindowsAzure.Commands.Sync
 {
-    using Internal.Common;
     using Microsoft.WindowsAzure.Storage;
     using Sync.Upload;
     using System;
@@ -39,7 +38,8 @@ namespace Microsoft.WindowsAzure.Commands.Sync
             }
         }
 //        static public IProgramConfiguration Configuration { get; private set; }
-        private static ISyncOutputEvents RawEvents = new SyncOutputEvents();
+        //private static ISyncOutputEvents RawEvents = new SyncOutputEvents();
+        private static ISyncOutputEvents RawEvents = null;
     }
 
     public interface ISyncOutputEvents
@@ -78,181 +78,5 @@ namespace Microsoft.WindowsAzure.Commands.Sync
         void ProgressEmptyBlockDetection(int processedRangeCount, int totalRangeCount);
 
         void WriteVerboseWithTimestamp(string message, params object[] args);
-    }
-
-    internal class SyncOutputEvents : ConsoleApplicationStandardOutputEvents, ISyncOutputEvents
-    {
-        public void ProgressUploadStatus(ProgressRecord record)
-        {
-            ProgressUploadStatus(record.PercentComplete, record.AvgThroughputMbPerSecond, record.RemainingTime);
-        }
-
-        private string FormatDuration(TimeSpan ts)
-        {
-            if (ts.Days == 0)
-            {
-                return String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
-            }
-            else
-            {
-                return String.Format("{0} days {1:00}:{2:00}:{3:00}", ts.Days, ts.Hours, ts.Minutes, ts.Seconds);
-            }
-        }
-        public void ProgressUploadStatus(double precentComplete, double avgThroughputMbps, TimeSpan remainingTime)
-        {
-            LogProgress("Uploading: {0:0.0}% complete; Remaining Time: {1}; Throughput: {2:0.0}Mbps",
-                precentComplete,
-                FormatDuration(remainingTime),
-                avgThroughputMbps);
-        }
-
-        public void MessageCreatingNewPageBlob(long pageBlobSize)
-        {
-            LogMessage("Creating new page blob of size {0}...", pageBlobSize);
-        }
-
-        public void MessageResumingUpload()
-        {
-            LogMessage("Found existing page blob. Resuming upload...");
-        }
-
-        public void ProgressUploadComplete(TimeSpan elapsed)
-        {
-            LogProgress("");
-            LogMessage("Elapsed time for upload: {0}", 
-                FormatDuration(elapsed));
-        }
-
-        public void ProgressDownloadStatus(ProgressRecord record)
-        {
-            ProgressDownloadStatus(record.PercentComplete, record.AvgThroughputMbPerSecond, record.RemainingTime);
-        }
-
-        public void ProgressDownloadStatus(double precentComplete, double avgThroughputMbps, TimeSpan remainingTime)
-        {
-            LogProgress("Downloading: {0:0.0}% complete; Remaining Time: {1}; Throughput: {2:0.0}Mbps",
-                precentComplete,
-                FormatDuration(remainingTime),
-                avgThroughputMbps);
-        }
-
-        public void ProgressDownloadComplete(TimeSpan elapsed)
-        {
-            LogProgress("");
-            LogMessage("Elapsed time for download: {0}",
-                FormatDuration(elapsed));
-        }
-
-        public void ProgressOperationStatus(ProgressRecord record)
-        {
-            ProgressOperationStatus(record.PercentComplete, record.AvgThroughputMbPerSecond, record.RemainingTime);
-        }
-
-        public void ProgressOperationStatus(double percentComplete, double avgThroughputMbps, TimeSpan remainingTime)
-        {
-            LogProgress("Progressing: {0:0.0}% complete; Remaining Time: {1}; Throughput: {2:0.0}Mbps",
-                percentComplete,
-                FormatDuration(remainingTime),
-                avgThroughputMbps);
-        }
-
-        public void ProgressOperationComplete(TimeSpan elapsed)
-        {
-            LogProgress("");
-            LogMessage("Elapsed time for the operation: {0}",
-                FormatDuration(elapsed));
-        }
-
-        public void ProgressCopyStatus(ProgressRecord record)
-        {
-            ProgressCopyStatus(record.PercentComplete, record.AvgThroughputMbPerSecond, record.RemainingTime);
-        }
-
-        public void ProgressCopyComplete(TimeSpan elapsed)
-        {
-            LogProgress("");
-            LogMessage("Elapsed time for the copy: {0}",
-                FormatDuration(elapsed));
-        }
-
-        public void ProgressCopyStatus(double precentComplete, double avgThroughputMbps, TimeSpan remainingTime)
-        {
-            LogProgress("Progressing: {0:0.0}% complete; Remaining Time: {1}; Throughput: {2:0.0}Mbps",
-                precentComplete,
-                FormatDuration(remainingTime),
-                avgThroughputMbps);
-        }
-
-        public void ErrorUploadFailedWithExceptions(IList<Exception> exceptions)
-        {
-            LogError("Upload failed with exceptions:");
-            foreach (var exception in exceptions)
-            {
-                LogError(exception.ToString());
-            }
-        }
-
-        public void MessageCalculatingMD5Hash(string filePath)
-        {
-            LogMessage("MD5 hash is being calculated for the file '{0}'.", filePath);
-        }
-
-        public void MessageMD5HashCalculationFinished()
-        {
-            LogMessage("MD5 hash calculation is completed.");
-        }
-
-        public void MessageRetryingAfterANetworkDisruption()
-        {
-            LogMessage("Network disruption occured, retrying.");
-        }
-
-        public void DebugRetryingAfterException(Exception lastException)
-        {
-            LogDebug(lastException.ToString());
-            
-            var storageException = lastException as StorageException;
-            var message = ExceptionUtil.DumpStorageExceptionErrorDetails(storageException);
-            if(message != String.Empty)
-            {
-                LogDebug(message);
-            }
-        }
-
-        public void MessageDetectingActualDataBlocks()
-        {
-            LogMessage("Detecting the empty data blocks in the local file.");
-        }
-
-        public void MessageDetectingActualDataBlocksCompleted()
-        {
-            LogMessage("Detecting the empty data blocks completed.");
-        }
-
-        public void MessagePrintBlockRange(IndexRange range)
-        {
-            LogMessage("Range of the block is {0}, Length: {1}", range, range.Length);
-        }
-
-        public void DebugEmptyBlockDetected(IndexRange range)
-        {
-            LogDebug("Empty block detected: {0}", range.ToString());
-        }
-
-        public void ProgressEmptyBlockDetection(int processedRangeCount, int totalRangeCount)
-        {
-            if (processedRangeCount >= totalRangeCount)
-            {
-                LogProgress("");
-                LogMessage("");
-                return;
-            }
-            LogProgress("Total block count: {0}, Processed block count: {1}", totalRangeCount, processedRangeCount);
-        }
-
-        public void WriteVerboseWithTimestamp(string message, params object[] args)
-        {
-            LogMessage(message, args);
-        }
     }
 }
